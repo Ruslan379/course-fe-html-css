203176197eedafc96b4cb8066507305c1e524e9d
Ruslan <arnov@i.ua>
2024-05-06 14:26:09 +0300

    Next 900 - Finished project Antools

---
+++ projects/antools/js/slider.js
@@ -0,0 +1,86 @@
+const slider = document.querySelector('.slider');
+const prevBtn = document.getElementById('prevBtn');
+const nextBtn = document.getElementById('nextBtn');
+// Задаємо початковий стан (колір) кнопок < та >
+prevBtn.querySelector('path').setAttribute('stroke', 'rgba(255, 255, 255, 0.28)');
+nextBtn.querySelector('path').setAttribute('stroke', '#FF6E30');
+prevBtn.disabled = true; // Робимо кнопку < неактивной
+const slides = document.querySelectorAll('.slider .team-item');
+// Пагінація
+const paginationFirst = document.getElementById('first');
+const paginationSecond = document.getElementById('second');
+const paginationThird = document.getElementById('third');
+const slideWidth = slides[0].clientWidth;
+console.log(slideWidth);
+let currentIndex = 0;
+
+function slideRight() {
+    currentIndex++;
+    if (currentIndex >= slides.length) {
+        currentIndex = 0;
+        console.log(currentIndex);
+    }
+    updateSlider();
+}
+
+function slideLeft() {
+    currentIndex--;
+    if (currentIndex < 0) {
+        currentIndex = slides.length - 1;
+    }
+    updateSlider();
+}
+
+function updateSlider() {
+    const newPosition = currentIndex * slideWidth;
+    slider.style.transform = `translateX(${newPosition}px)`;
+    // +++++ зміна стану та кольору копок "<" і ">" та Пагінація
+    if (currentIndex === 0) {
+        // зміна кольору:
+        prevBtn.querySelector('path').setAttribute('stroke', 'rgba(255, 255, 255, 0.28)');
+        nextBtn.querySelector('path').setAttribute('stroke', '#FF6E30');
+        // зміна стану:
+        prevBtn.disabled = true; // Делаем кнопку неактивной
+        nextBtn.disabled = false; // Разблокируем следующую кнопку
+        // Пагінація:
+        paginationFirst.style.height = '8px';
+        paginationFirst.style.backgroundColor = '#FF6E30';
+        paginationSecond.style.height = '6px';
+        paginationSecond.style.backgroundColor = '#283036';
+        paginationThird.style.height = '6px';
+        paginationThird.style.backgroundColor = '#283036';
+
+    } else if (currentIndex === 1) {
+        // зміна кольору:
+        prevBtn.querySelector('path').setAttribute('stroke', '#FF6E30');
+        nextBtn.querySelector('path').setAttribute('stroke', '#FF6E30');
+        // зміна стану:
+        prevBtn.disabled = false; // Разблокируем предыдущую кнопку
+        nextBtn.disabled = false; // Разблокируем следующую кнопку
+        // Пагінація:
+        paginationFirst.style.height = '6px';
+        paginationFirst.style.backgroundColor = '#283036';
+        paginationSecond.style.height = '8px';
+        paginationSecond.style.backgroundColor = '#FF6E30';
+        paginationThird.style.height = '6px';
+        paginationThird.style.backgroundColor = '#283036';
+    } else if (currentIndex === 2) {
+        // зміна кольору:
+        prevBtn.querySelector('path').setAttribute('stroke', '#FF6E30');
+        nextBtn.querySelector('path').setAttribute('stroke', 'rgba(255, 255, 255, 0.28)');
+        // зміна стану:
+        prevBtn.disabled = false; // Разблокируем предыдущую кнопку
+        nextBtn.disabled = true; // Делаем кнопку неактивной
+        // Пагінація:
+        paginationFirst.style.height = '6px';
+        paginationFirst.style.backgroundColor = '#283036';
+        paginationSecond.style.height = '6px';
+        paginationSecond.style.backgroundColor = '#283036';
+        paginationThird.style.height = '8px';
+        paginationThird.style.backgroundColor = '#FF6E30';
+    }
+    // +++++
+}
+
+prevBtn.addEventListener('click', slideLeft);
+nextBtn.addEventListener('click', slideRight);
