337e3149bc085336c6738b9bf50a0caf15a03b75
Ruslan379 <arnov@i.ua>
2024-11-20 17:28:14 +0200

    Next 1327-JS

--- lesson-FE3_01/js/02-types.js
+++ lesson-FE3_01/js/02-types.js
@@ -20,7 +20,6 @@ const username = 'Mango995';
 const description = "JavaScript is awesome!";
 // console.log('username:', username); // Mango995
 
-
 //? Boolean   — логічний тип даних, прапорці стану.
 const isOpen = true;
 const shouldConfirm = false;
@@ -29,12 +28,12 @@ const isLoggedIn = false;
 // console.log('isLoggedIn:', isLoggedIn); // false
 
 
+//! 4-2.Спеціальні значення
 //? null — особливе значення, яке по суті означає ніщо. 
 //? Використовується в тих ситуаціях, коли необхідно 
 //? явно вказати порожнечу. 
 let value = null;
 // console.log('value:', value); // null
-
 
 //? undefined - значення на даний момент невідоме чи невизначене.
 //? змінна була оголошена, але ще не була ініціалізована значенням
@@ -42,50 +41,56 @@ let myValue;
 // console.log('myValue:', myValue); // undefined
 
 
-
+//! 4-3.Оператор typeof
 /*
- * * - Оператор typeof .....................
- */
+* Використовується для отримання типу значення змінної. 
+* повертає на місце свого виклику тип значення змінної, 
+* вказаного після нього — рядок, в якому вказано тип.
+*/
 const quantity = 17;
-// console.log(typeof quantity); // виведе "number"
+console.log('typeof quantity =', typeof quantity); // виведе "number"
 
 const myMessage = "JavaScript is awesome!";
-// console.log(typeof myMessage); // виведе "string"
+console.log('typeof myMessage =', typeof myMessage); // виведе "string"
 
 const isSidebarOpen = false;
-// console.log(typeof isSidebarOpen); // виведе "boolean"
+console.log('typeof isSidebarOpen =', typeof isSidebarOpen); // виведе "boolean"
 
 let userAge;
-// console.log(typeof userAge); // виведе "undefined"
+console.log('typeof userAge =', typeof userAge); // виведе "undefined"
 
 const type = typeof isOpen;
-
-// console.log(type);
+console.log('type:', type); // виведе "boolean"
 
 let myStatus = null;
-// console.log(typeof myStatus); // виведе "object"
-// в ранніх версіях JavaScript null було розглянуто як спеціальний випадок об’єкта.
-//  Це була помилка в реалізації мови, збережена для 
-//  забезпечення зворотної сумісності з існуючим кодом
+console.log('typeof myStatus =', typeof myStatus); // виведе "object"
+//?  В ранніх версіях JavaScript null було розглянуто 
+//?  як спеціальний випадок об’єкта.
+//?  Це була помилка в реалізації мови, збережена для 
+//?  забезпечення зворотної сумісності з існуючим кодом
 
-//BigInt
+
+
+//? BigInt
 /*
-В JavaScript тип данних «number» не може
-містити числа більше, ніж 9007199254740991,
-чи менше, ніж -9007199254740991.
-ECMAScript 2020 (ES11). Він дозволяє працювати з цілими числами, 
-які можуть бути більшими за максимальне значення
+* В JavaScript тип данних «number» не може
+* містити числа більше, ніж 9007199254740991,
+* чи менше, ніж -9007199254740991.
+* ECMAScript 2020 (ES11). Він дозволяє працювати з цілими числами, 
+* які можуть бути більшими за максимальне значення
 */
-//BigInt
 const bigInteger = 12345678901234567890123456789n;
-// console.log(typeof bigInteger);
+console.log('typeof bigInteger =', typeof bigInteger); // виведе "bigint"
 
 
-//Symbol
-// Символи використовуються для створення унікальних ідентифікаторів,
-//  що не можуть бути випадково переплутані з іншими ідентифікаторами,
+//? Symbol
+/*
+* Символи використовуються для створення унікальних 
+* ідентифікаторів, що не можуть бути випадково
+* переплутані з іншими ідентифікаторами,
+*/
 let id = Symbol("id");
-// console.log(typeof id);
+console.log('typeof id =', typeof id); // виведе "symbol"
 
 
 // alert.....................................................
