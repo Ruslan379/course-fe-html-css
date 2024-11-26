a6b716a633ef095f62d81b05b447e6d1589905d3
Ruslan <arnov@i.ua>
2024-11-26 19:17:04 +0200

    Next 1366-JS

--- lesson-FE3_01/js/05-input.js
+++ lesson-FE3_01/js/05-input.js
@@ -1,54 +1,97 @@
 console.warn('5.Взаємодія з користувачем. Отримання даних.');
-//! alert - виведення даних в модальне вікно
+
+//todo Методи window.alert(), window.confirm(), window.prompt()
+//! alert() - виведення даних в модальне вікно
 //! confirm() - oтримання даних з модального вікна
-//! prompt() - oтримання даних з модального вікна
+//! prompt() - oтримання додаткових даних з модального вікна
 //! ___________________________
 
-//! alert
-const myMmessage = "JavaScript is awesome!";
+
+//! alert()
+//? Виводить модальне вікно з ПОВІДОМЛЕННЯМ і КНОПКОЮ [OK] та чекає підтвердження.
+const myMmessage = "alert() --> JavaScript is awesome!";
 console.log(myMmessage);
 alert(myMmessage);
+console.log("---------------------------------");
 
-//* Приклад того як window.alert() блокує інтерпретацію
-console.log('До');
-alert('УВАГА!');
-console.log('Після');
+//?o Приклад того як alert() блокує інтерпретацію(компіляцію)
+console.log('До запуска alert()');
+alert('alert() --> Зверніть увагу на консоль!');
+//todo Тут код припиняє свою роботу до
+//todo підтерження повідомленя від alert(),
+//todo тобто до натискання на кнопку[OK]
+console.log('Після виконання alert()');
+console.log("---------------------------------");
 
-
-//todo Методи window.confirm() та window.prompt()
 
 //! confirm() - oтримання даних з модального вікна
-//? Виводить модальне вікно з повідомленням, і дві кнопки, Ok і Cancel.
-//? Натискаючи на Ok, результатом будет true,
-//? Натискаючи на Cancel — повертається false.
-//todo Просимо клієнта підтвердити бронювання готелю
-//todo і зберігаємо результат роботи confirm у змінну
-const isComing = confirm("Please confirm hotel reservation");
-console.log('isComing:', isComing);
+//? Виводить модальне вікно з ПОВІДОМЛЕННЯМ і КНОПКАМИ [OK] і [Cancel].
+//? Натискаючи на [OK] отримуємо в змінну isComing значення = true,
+//? Натискаючи на [Cancel] отримуемо в змінну isComing значення = false.
+//todo ПРИКЛАД: Просимо клієнта підтвердити бронювання готелю
+//todo і зберігаємо результат роботи confirm у змінну isComing
+let isComing = confirm("confirm() --> Please confirm hotel reservation_[OK]");
+console.log('isComing_[OK]:', isComing);
+isComing = confirm("confirm() --> Please confirm hotel reservation_[Cancel]");
+console.log('isComing_[Cancel]:', isComing);
+console.log("---------------------------------");
 
 
-//! prompt() - oтримання даних з модального вікна
-//? Виводить модальне вікно з полем введення і кнопками Ok і Cancel.
-//? Повертається Ok - що ввів користувач, у випадку Cancel — null.
+//! prompt() - oтримання додаткових даних з модального вікна
+//? Виводить модальне вікно з ПОВІДОМЛЕННЯМ та ПОЛЕМ ВВЕДЕННЯ ДАНИХ і КНОПКАМИ [OK] і [Cancel].
+//? Отримуємо в змінну hotelName значення = ТЕ, що  користувач ввів в ПОЛЕ ВВЕДЕННЯ ДАНИХ.
+//? Отримуємо в змінну hotelName значення = пустий рядок - якщо користувач НЕ ввів дані та натиснув КНОПКУ [OK].
+//? Отримуємо в змінну hotelName значення = null - якщо користувач НЕ ввів/ввів дані та натиснув КНОПКУ [Cancel].
 //todo Запитуємо назву готеля, в якому хотів би зупинитися клієнт
 //todo і зберігаємо результат виклику prompt у змінну.
-const hotelName = prompt("Please enter desired hotel name");
-console.log('hotelName:', hotelName);
+let hotelName = prompt("prompt() --> Please enter desired hotel name_(name+[OK])");
+console.log('hotelName_(name+[OK]):', hotelName);
+
+hotelName = prompt("prompt() --> Please enter desired hotel name_[OK]");
+console.log('hotelName_[OK]:', hotelName);
+
+hotelName = prompt("prompt() --> Please enter desired hotel name_[Cancel]");
+console.log('hotelName_[Cancel]:', hotelName);
+console.log("---------------------------------");
 
 
 //todo Важлива особливість prompt полягає в тому,
 //todo  що незалежно від того, що ввів користувач,
-//todo завжди повернеться рядок
-let quantity = prompt('Введіть кількість товарів');
-console.log('quantity-1:', quantity);
-console.log('typeof quantity =', typeof quantity);
+//todo тип значення змінної буде - рядок(string)!
+let quantity = prompt('prompt() --> Введіть кількість товарів');
+console.log('quantity(before)):', quantity);
+console.log('typeof quantity(before) =', typeof quantity);
 
-//todo Зміна типу зі string на number
+//todo Перетворюємо тип значення змінної quantity зі string на number:
 quantity = Number(quantity);
-console.log('quantity-2:', quantity);
-console.log('typeof quantity-2 =', typeof quantity);
+console.log('quantity(after)):', quantity);
+console.log('typeof quantity(after) =', typeof quantity);
+console.log("---------------------------------");
 
+//todo Перевірка на NaN - метод Number.isNaN(val)
 
+function сheckForNumber() { 
+    const itsNotAnumber = Number.isNaN(quantity);
+    console.log('itsNotAnumber:', itsNotAnumber);
+
+    if (itsNotAnumber) {
+        alert(" ПОМИЛКА!\n Це не число!\n Введіть ЧИСЛО!");
+        quantity = prompt('prompt() --> Введіть кількість товарів ще раз');
+        console.log('quantity(before)):', quantity);
+        console.log('typeof quantity(before) =', typeof quantity);
+
+        //todo Перетворюємо тип значення змінної quantity зі string на number:
+        quantity = Number(quantity);
+        console.log('quantity(after)):', quantity);
+        console.log('typeof quantity(after) =', typeof quantity);
+        console.log("---------------------------------");
+    }
+    else {
+        alert("Кількість товару підтверджено!");
+        return;
+    } 
+};
+сheckForNumber();
 
 
 
