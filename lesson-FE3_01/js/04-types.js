52353960c321cb1f2020721e2b439b7bf098941c
Ruslan <arnov@i.ua>
2024-11-26 12:58:52 +0200

    Next 1364-JS

--- lesson-FE3_01/js/04-types.js
+++ lesson-FE3_01/js/04-types.js
@@ -1,125 +1,123 @@
 console.warn('4.Tипи даних. Спеціальні значення. Оператор typeof.');
 
-//! 4-1.Tипи даних
-//! 4-2.Спеціальні значення
-//! 4-3.Оператор typeof
+//! Tипи даних
+//! Спеціальні значення типу даних
+//! Оператор typeof
+//! BigInt - спеціальний тип даних
+//! Symbol - спеціальний тип даних
 //! ___________________________
 
 
-//! 4-1.Tипи даних
-//? Number —  цілі числа і числа з плаваючою комою (крапкою).
+//! Tипи даних
+//? Number —  цілі числа і(або) числа з плаваючою комою(крапкою).
 const myAge = 10;
 const totalPrice = 200.74;
-// const totalPrice = 200,74;
+// const totalPrice = 200,74; //! ❌ Неправильно, буде помилка!
+console.log('totalPrice:', totalPrice); //! totalPrice: 200.74
 
-//? String   — рядки, послідовність з нуля або більше символів. 
+//? String   — рядки, послідовність з нуля("") або більше символів. 
 //? Рядок починається і закінчується одинарними ', або подвійними лапками ".
-const userName = 'Chelsy';
+const userName = 'Chelsy'; //! userName i username - це різні імена змінних
 const message = 'welcome!';
-const username = 'Mango995';
+const username = 'Mango-369'; //! userName i username - це різні імена змінних
 const description = "JavaScript is awesome!";
-// console.log('username:', username); // Mango995
+console.log('userName:', userName); //! userName: Chelsy
+console.log('username:', username); //! Mango-369
 
 //? Boolean   — логічний тип даних, прапорці стану.
+//? Має лише два значеня: true або false (правда або брехня)
 const isOpen = true;
 const shouldConfirm = false;
 const isModalOpen = true;
 const isLoggedIn = false;
-// console.log('isLoggedIn:', isLoggedIn); // false
+console.log('isLoggedIn:', isLoggedIn); //! false
 
 
-//! 4-2.Спеціальні значення
-//? null — особливе значення, яке по суті означає ніщо. 
+//! Спеціальні значення типу даних
+//? null — особливе значення, яке по суті означає НІЩО. 
 //? Використовується в тих ситуаціях, коли необхідно 
 //? явно вказати порожнечу. 
 let value = null;
-// console.log('value:', value); // null
+console.log('value:', value); //! null
 
 //? undefined - значення на даний момент невідоме чи невизначене.
-//? змінна була оголошена, але ще не була ініціалізована значенням
+//? undefined буде тоді, коли змінна була оголошена, 
+//? але ще не була ініціалізована значенням
 let myValue;
-// console.log('myValue:', myValue); // undefined
+console.log('myValue:', myValue); //! undefined
+console.log("-----------------------");
 
 
-//! 4-3.Оператор typeof
-/*
-* Використовується для отримання типу значення змінної. 
-* повертає на місце свого виклику тип значення змінної, 
-* вказаного після нього — рядок, в якому вказано тип.
-*/
+//! Оператор typeof
+//? Використовується для отримання типу значення змінної. 
+//? повертає на місце свого виклику тип значення змінної, 
+//? вказаного після нього — рядок, в якому вказано тип.
 const quantity = 17;
-// console.log('typeof quantity =', typeof quantity); // виведе "number"
+console.log('typeof quantity =', typeof quantity); //! typeof quantity = number
 
 const myMessage = "JavaScript is awesome!";
-// console.log('typeof myMessage =', typeof myMessage); // виведе "string"
+console.log('typeof myMessage =', typeof myMessage); //! typeof myMessage = string
 
 const isSidebarOpen = false;
-// console.log('typeof isSidebarOpen =', typeof isSidebarOpen); // виведе "boolean"
+console.log('typeof isSidebarOpen =', typeof isSidebarOpen); //! typeof isSidebarOpen = boolean
+
+const isClose = true;
+const typeIsClose = typeof isClose;
+console.log('typeIsClose:', typeIsClose); //! type: boolean
 
 let userAge;
-// console.log('typeof userAge =', typeof userAge); // виведе "undefined"
+console.log('typeof userAge =', typeof userAge); //! typeof userAge = undefined
 
-const type = typeof isOpen;
-// console.log('type:', type); // виведе "boolean"
-
-let myStatus = null;
-// console.log('typeof myStatus =', typeof myStatus); // виведе "object"
-//?  В ранніх версіях JavaScript null було розглянуто 
+//?  В ранніх версіях JavaScript null було розглянуто
 //?  як спеціальний випадок об’єкта.
-//?  Це була помилка в реалізації мови, збережена для 
+//?  Це була помилка в реалізації мови, збережена для
 //?  забезпечення зворотної сумісності з існуючим кодом
+let myStatus = null;
+console.log('typeof myStatus =', typeof myStatus); // виведе "object"
+console.log("-----------------------");
 
 
-
-//? BigInt
-/*
-* В JavaScript тип данних «number» не може
-* містити числа більше, ніж 9007199254740991,
-* чи менше, ніж -9007199254740991.
-* ECMAScript 2020 (ES11). Він дозволяє працювати з цілими числами, 
-* які можуть бути більшими за максимальне значення
-*/
+//! BigInt - спеціальний тип даних
+//? В JavaScript тип данних «number» не може
+//? містити числа більше, ніж 9007199254740991,
+//? чи менше, ніж -9007199254740991.
+//? ECMAScript 2020 (ES11)дозволяє працювати з цілими числами,
+//? які можуть бути більшими за максимальне значення
+//? використовуючи тип даних - BigInt
 const bigInteger = 12345678901234567890123456789n;
-// console.log('typeof bigInteger =', typeof bigInteger); // виведе "bigint"
+console.log('typeof bigInteger =', typeof bigInteger); // виведе "bigint"
 
 
-//? Symbol
-/*
-* Символи використовуються для створення унікальних 
-* ідентифікаторів, що не можуть бути випадково
-* переплутані з іншими ідентифікаторами,
-*/
-let id = Symbol("id");
-// console.log('typeof id =', typeof id); // виведе "symbol"
-
+//! Symbol - спеціальний тип даних
+//? Symbol використовуються для створення унікальних 
+//? ідентифікаторів, що не можуть бути випадково
+//? переплутані з іншими ідентифікаторами,
+const id = Symbol("myId");
+console.log('typeof id =', typeof id); //! typeof id = symbol
 
 
 
 
 
-//! практика
-//? 1
-// Оголоси наступні змінні, використовуючи ключове слово const 
-//  і присвой їм відповідні значення.
-
-// topSpeed - число 160
-// distance - число 617.54
-// login - рядок "mango935"
-// isOnline - буль true
-// isAdmin - буль false
 
 
-// const age = 10;
-// const firstName = 'Chelsy';
-// const isVIP = true;
-// console.log(age);
-// console.log(firstName);
-// console.log(isVIP);
-
-// console.log(typeof 'Chelsy');
-
-// console.log('This is a number: ', age);
+//! Практика
+//todo [1]
+//? Оголоси наступні змінні, використовуючи ключове слово const
+//?  і присвой їм відповідні значення.
+//? Результат вивести у консоль.
+//* topSpeed - число 160
+//* distance - число 617.54
+//* login - рядок "mango935"
 
 
-//? 2
-// Оголоси змінну з привітанням, виведи її через alert()
+//todo [2]
+//? Оголоси наступні змінні, використовуючи ключове слово let
+//?  і присвой їм відповідні значення.
+//? Результат вивести у консоль.
+//* isOnline - буль true
+//* isAdmin - буль false
+//? Потім перевизначити значення на протилежні.
+//? Результат вивести у консоль.
+
+
--- lesson-FE3_01/js/05-input.js
+++ lesson-FE3_01/js/05-input.js
@@ -1,59 +1,65 @@
 console.warn('5.Взаємодія з користувачем. Отримання даних.');
+//! alert - виведення даних в модальне вікно
+//! confirm() - oтримання даних з модального вікна
+//! prompt() - oтримання даних з модального вікна
+//! ___________________________
 
 //! alert
 const myMmessage = "JavaScript is awesome!";
-// console.log(myMmessage);
-// alert(myMmessage);
+console.log(myMmessage);
+alert(myMmessage);
 
 //* Приклад того як window.alert() блокує інтерпретацію
-// console.log('До');
-// alert('УВАГА!');
-// console.log('Після');
+console.log('До');
+alert('УВАГА!');
+console.log('Після');
 
 
+//todo Методи window.confirm() та window.prompt()
 
-//! Отримання даних
-//* Методи window.confirm() та window.prompt()
-
-//! confirm() 
-//? Виводить модальне вікно з повід, і дві кнопки, Ok і Cancel. 
-//? Натискаючи на Ok, результатом будет true, 
+//! confirm() - oтримання даних з модального вікна
+//? Виводить модальне вікно з повід, і дві кнопки, Ok і Cancel.
+//? Натискаючи на Ok, результатом будет true,
 //? Натискаючи на Cancel — повертається false.
-
 //todo Просимо клієнта підтвердити бронювання готелю
 //todo і зберігаємо результат роботи confirm у змінну
-// const isComing = confirm("Please confirm hotel reservation");
-// console.log('isComing:', isComing);
+const isComing = confirm("Please confirm hotel reservation");
+console.log('isComing:', isComing);
 
 
-//! prompt()  
+//! prompt() - oтримання даних з модального вікна
 //? Виводить модальне вікно з полем введення і кнопками Ok і Cancel.
 //? Повертається Ok - що ввів користувач, у випадку Cancel — null.
-
 //todo Запитуємо назву готеля, в якому хотів би зупинитися клієнт
 //todo і зберігаємо результат виклику prompt у змінну.
+const hotelName = prompt("Please enter desired hotel name");
+console.log('hotelName:', hotelName);
 
-// const hotelName = prompt("Please enter desired hotel name");
-// console.log('hotelName:', hotelName);
 
+//todo Важлива особливість prompt полягає в тому,
+//todo  що незалежно від того, що ввів користувач,
+//todo завжди повернеться рядок
+let quantity = prompt('Введіть кількість товарів');
+console.log('quantity-1:', quantity);
+console.log('typeof quantity =', typeof quantity);
 
-//? Важлива особливість prompt полягає в тому,
-//?  що незалежно від того, що ввів користувач, 
-//? завжди повернеться рядок
-// let quantity = prompt('Введіть кількість товарів');
-// console.log('quantity-1:', quantity);
-// console.log('typeof quantity =', typeof quantity);
-
-//? Зміна типу зі string на number
-// quantity = Number(quantity);
-// console.log('quantity-2:', quantity);
-// console.log('typeof quantity-2 =', typeof quantity);
-
+//todo Зміна типу зі string на number
+quantity = Number(quantity);
+console.log('quantity-2:', quantity);
+console.log('typeof quantity-2 =', typeof quantity);
 
 
 
-//! практика
 
+
+
+
+//! Практика
+//todo [1]
+// Оголоси змінну з привітанням, виведи її через alert()
+
+
+//todo [2]
 // const shouldRenew = confirm('Хочете продовжити підпистку?');
 // console.log(shouldRenew);
 
@@ -62,7 +68,7 @@ const myMmessage = "JavaScript is awesome!";
 //     } else {
 //         alert("Дію відмінено!");
 //     }
-    
+
 
 // const message = "введіть імя";
 // const clientName = prompt(message);
@@ -89,3 +95,4 @@ const myMmessage = "JavaScript is awesome!";
 // } else {
 //     alert("Спробуйте ще раз");
 // }
+
