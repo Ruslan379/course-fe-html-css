f417c120b89ba064737c6552020b8db08e6c3363
Ruslan <arnov@i.ua>
2024-11-27 14:32:20 +0200

    Next 1371-JS

--- lesson-FE3_02/js/03-strict-non-strict-equality.js
+++ lesson-FE3_02/js/03-strict-non-strict-equality.js
@@ -70,41 +70,41 @@ console.log("1 != true:", 1 !== true); //! true
 // console.log(resultTwo);
 // console.log(typeof resultTwo);
 
-// Пріорітет операторів
+//? Пріорітет операторів
 
-// Різний пріорітет
+//? Різний пріорітет
 // let resultOne = 2 - 1 * 5;
 // console.log(resultOne);
 
-// Скобки
+//? Скобки
 // let resultTwo = (2 - 1) * 5;
 // console.log(resultTwo);
 
-//Однаковий пріорітет
+//? Однаковий пріорітет
 // let resultThree = 2 - 1 + 5;
 // console.log(resultThree);
 
 
 //todo [1]
-// Доповни код, присвоївши змінній totalPrice вираз для підрахунку 
-// загальної суми замовлення. Змінна pricePerItem зберігає ціну за одиницю товару,
-//  а orderedQuantity - кількість одиниць товару в замовленні.
+//? Доповни код, присвоївши змінній totalPrice вираз для підрахунку 
+//? загальної суми замовлення. Змінна pricePerItem зберігає ціну за одиницю товару,
+//?  а orderedQuantity - кількість одиниць товару в замовленні.
 
-// const pricePerItem = 3500;
-// const orderedQuantity = 4;
+//? const pricePerItem = 3500;
+//? const orderedQuantity = 4;
 
-// const totalPrice = pricePerItem * orderedQuantity;
+//? const totalPrice = pricePerItem * orderedQuantity;
 
 
 //todo [2]
-// Заміни у виразах стандартні математичні оператори на комбіновані оператори
+//? Заміни у виразах стандартні математичні оператори на комбіновані оператори
 
 // let a = 5;
 // let b = 10;
 let c = 15;
 let d = 20;
 
-// Change code below this line
+//? Change code below this line
 // a = a + 2;
 // b = b - 4;
 // c = c * 3;
@@ -117,26 +117,23 @@ let d = 20;
 
 
 //todo [3]
-// Оголоси змінну isAdult та задай їй результат перевірки значення змінної age.
-//  Використай оператори порівняння та доповни код так, щоб 
-//  значенням isAdult було true, якщо вік користувача більше або дорівнює 18 
-//  та false, якщо менше 18.
-
+//? Оголоси змінну isAdult та задай їй результат перевірки значення змінної age.
+//?  Використай оператори порівняння та доповни код так, щоб 
+//?  значенням isAdult було true, якщо вік користувача більше або дорівнює 18 
+//?  та false, якщо менше 18.
 // const age = 16;
 // const isAdult = age >= 18;
 
 
 //todo [4]
-// Оголоси змінну isValid і задай їй значенням результат перевірки
-//  рівності паролей в змінних correctPassword і userPassword.
-
-// Якщо паролі співпадають (сувора рівність), значення isValid має бути true
-// Якщо паролі не співпадають, значення isValid має бути false
+//? Оголоси змінну isValid і задай їй значенням результат перевірки
+//?  рівності паролей в змінних correctPassword і userPassword.
+//? Якщо паролі співпадають (сувора рівність), значення isValid має бути true
+//? Якщо паролі не співпадають, значення isValid має бути false
 
 const correctPassword = "jqueryismyjam";
 const userPassword = "mangodab3st";
 const isValid = correctPassword === userPassword;
-
 
 // let a = 1 + 2;
 // let b = 2;
