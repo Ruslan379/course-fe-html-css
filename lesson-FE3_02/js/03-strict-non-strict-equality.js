49c05d27f1e2cbe1efc4d88a620f968ef9e60058
Ruslan <arnov@i.ua>
2024-11-27 16:25:47 +0200

    Next 1379-JS

--- lesson-FE3_02/js/03-strict-non-strict-equality.js
+++ lesson-FE3_02/js/03-strict-non-strict-equality.js
@@ -41,6 +41,8 @@ console.log('5 == "5":', 5 == "5"); //! true
 console.log('5 != "5":', 5 != "5"); //! false 
 //* Boolean
 console.log("1 == true:", 1 == true); //! true
+console.log('"1" == true:', "1" == true); //! true
+console.log("[1] == true:", [1] == true); //! true
 console.log("1 != true:", 1 != true); //! false
 
 
