1a50e846014e37244379764229b5568f1e74e71c
Ruslan <arnov@i.ua>
2024-12-01 21:28:09 +0200

    Next 1392-JS

--- lesson-FE3_03/js/02-concatenation-strings.js
+++ lesson-FE3_03/js/02-concatenation-strings.js
@@ -4,21 +4,49 @@ console.log(
     'color: white; background-color: #D33F49',
 );
 
-//! Оператори порівняння
-//? Використовуються для порівняння двох значень і 
-//? повертають булеве значення (true або false)
+//! Конкатенація рядків
+//? Якщо застосувати оператор + до рядка та
+//? будь-якого іншого типу даних, 
+//? результатом буде новий рядок.
+const myMessage = "Mango " + "is " + "happy";
+console.log("myMessage:", myMessage); //! Mango is happy
 
-const a = 20;
-const b = 50;
-console.log("a =", a);
-console.log("b =", b);
-console.log("--------------");
 
-console.log("a > b:", a > b); //! false
-console.log("b > a:", b > a); //! true
-console.log("a >= b:", a >= b); //! false
-console.log("b >= a:", b >= a); //! true
-console.log("a < b:", a < b); //! true
-console.log("b < a:", b < a); //! false
-console.log("a <= b:", a <= b); //! true
-console.log("b <= a:", b <= a); //! false
+//? Під час конкатенації можна використовувати значення змінних, 
+//? щоб складати рядки з динамічними значеннями
+const age = 24;
+const newMessage = "Poly is " + age + " years old!";
+console.log("newMessage:", newMessage); //! Poly is 24 years old!
+console.log("-----------------------------------");
+
+
+//? Під час конкатенації будь-який тип даних
+//? буде приведено до рядка та поєднано з іншим рядком.
+console.log("'Mango' + 55:", 'Mango' + 55); //! Mango55
+console.log("'Mango' + true:", 'Mango' + true); //! Mangotrue
+console.log("-----------------------------------");
+
+
+//? Порядок операндів має значення.
+//? Перетворення типів відбувається лише в момент операції додавання з рядком.
+console.log("1 + '2':", 1 + '2'); //! 12
+console.log("1 + '2' + 4:", 1 + '2' + 4); //! 124
+console.log("1 + 2 + '4':", 1 + 2 + '4'); //! 34
+console.log("-----------------------------------");
+
+
+//todo Перетворення різних типів в рядки 
+//? Явне
+console.log("String(5):", String(5)); //! "5"
+console.log("String('5'):", String('5')); //! "5"
+console.log("String(true):", String(true)); //! "true"
+console.log("String(undefined):", String(undefined)); //! "undefined"
+console.log("String(null):", String(null)); //! "null"
+console.log("String(NaN):", String(NaN)); //! "NaN"
+console.log("-----------------------------------");
+
+//? Неявне - автоматично під час виконання операцій або обчислень
+console.log("5" + 3); //?"53"
+console.log("5" + true);
+console.log("5" + undefined); 
+console.log("-----------------------------------");
