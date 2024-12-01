80718e751e41ae56fa420e076f922b31df562ae2
Ruslan <arnov@i.ua>
2024-12-01 22:31:42 +0200

    Next 1395-JS

--- lesson-FE3_03/js/02-concatenation-strings.js
+++ lesson-FE3_03/js/02-concatenation-strings.js
@@ -9,14 +9,14 @@ console.log(
 //? будь-якого іншого типу даних, 
 //? результатом буде новий рядок.
 const myMessage = "Mango " + "is " + "happy";
-console.log("myMessage:", myMessage); //! "Mango is happy"
+console.log('("Mango " + "is " + "happy"):', myMessage); //! "Mango is happy"
 
 
 //? Під час конкатенації можна використовувати значення змінних, 
 //? щоб складати рядки з динамічними значеннями
 const age = 24;
 const newMessage = "Poly is " + age + " years old!";
-console.log("newMessage:", newMessage); //! "Poly is 24 years old!"
+console.log('("Poly is " + age + " years old!"):', newMessage); //! "Poly is 24 years old!"
 console.log("-----------------------------------");
 
 
@@ -28,25 +28,47 @@ console.log("-----------------------------------");
 
 
 //? Порядок операндів має значення.
-//? Перетворення типів відбувається лише в момент операції додавання з рядком.
+//? Перетворення типів відбувається лише 
+//? в момент операції додавання з рядком.
 console.log("1 + '2':", 1 + '2'); //! "12"
 console.log("1 + '2' + 4:", 1 + '2' + 4); //! "124"
 console.log("1 + 2 + '4':", 1 + 2 + '4'); //! "34"
-console.log("-----------------------------------");
 
 
-//todo Перетворення різних типів в рядки 
-//? Явне
+//todo Перетворення різних типів в рядки
+//? Явне - Функція-конструктор String(value)
+console.log(
+    "%c Явне приведення до рядка - String(value) ",
+    "color: yellow; background-color: #2274A5",
+);
 console.log("String(5):", String(5)); //! "5"
 console.log("String('5'):", String('5')); //! "5"
 console.log("String(true):", String(true)); //! "true"
 console.log("String(undefined):", String(undefined)); //! "undefined"
 console.log("String(null):", String(null)); //! "null"
 console.log("String(NaN):", String(NaN)); //! "NaN"
-console.log("-----------------------------------");
+
+
+//? Явне - метод .toString()
+console.log(
+    "%c Явне приведення до рядка - .toString() ",
+    "color: yellow; background-color: #2274A5",
+);
+console.log("(5).toString():", (5).toString()); //! "5"
+console.log("('5').toString():", ('5').toString()); //! "5"
+console.log("(true).toString:", (true).toString()); //! "true"
+// console.log("(undefined).toString:", (undefined).toString()); //! Cannot read properties of undefined (reading 'toString')
+console.error(`(undefined).toString: Cannot read properties of undefined (reading 'toString')`);
+// console.log("(null).toString:", (null).toString()); //! Cannot read properties of null (reading 'toString')
+console.error(`(null).toString: Cannot read properties of null (reading 'toString')`)
+console.log("(NaN).toString:", (NaN).toString()); //! "NaN"
 
 
 //? Неявне - автоматично під час виконання операцій або обчислень
+console.log(
+    "%c Неявне приведення до рядка - конкатенація(+) ",
+    "color: yellow; background-color: #2274A5",
+);
 console.log("'5' + 3:", '5' + 3); //! "53"
 console.log("3 + '5':", 3 + '5'); //! "35"
 console.log("'5' + true:", '5' + true); //! "5true"
