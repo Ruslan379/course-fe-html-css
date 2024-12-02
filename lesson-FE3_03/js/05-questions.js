a67a8672fa6afc7bd1ba9b1d18804e823f911eb9
Ruslan <arnov@i.ua>
2024-12-02 17:17:11 +0200

    Next 1411-JS

---
+++ lesson-FE3_03/js/05-questions.js
@@ -0,0 +1,10 @@
+// console.log('5.Запитання:');
+console.log(
+    "%c 5.Запитання: ",
+    "color: white; background-color: #D33F49",
+);
+
+//? Яке значення буде в консолі?
+
+
+
---
+++ lesson-FE3_03/js/06-practice.js
@@ -0,0 +1,9 @@
+// console.log('6.Практика:');
+console.log(
+  "%c 6.Практика: ",
+  "color: white; background-color: #D33F49",
+);
+
+//! ПРАКТИКА
+//! 2.Додаткові завдання
+//todo [1]
