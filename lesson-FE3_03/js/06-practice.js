a88be1b791d8e6efefd8764772bd9c7896ab10cd
Ruslan <arnov@i.ua>
2024-12-03 20:52:50 +0200

    Next 1415-JS

--- lesson-FE3_03/js/06-practice.js
+++ lesson-FE3_03/js/06-practice.js
@@ -4,6 +4,111 @@ console.log(
   "color: white; background-color: #D33F49",
 );
 
-//! ПРАКТИКА
-//! 2.Додаткові завдання
+
+//! ПРАКТИКА-3 (Урок-03_JS)
 //todo [1]
+console.log(
+  "%c [1] ",
+  "color: yellow; background-color: #2274A5",
+);
+//? Зробити змінну з ім'ям <userName> та будь яким 
+//? рядковим значенням, наприклад "Thomas Jeffrey Hanks"
+//? Знайти її довжину і записати у змінну <length>.
+//? Вивести в консоль повідомлення у такому форматі:
+//? "Username <userName> is <length> characters long"
+//! Код виконаного завдання
+
+
+
+//todo [2]
+console.log(
+  "%c [2] ",
+  "color: yellow; background-color: #2274A5",
+);
+//? Який індекс у першого елемента рядка?
+console.log("Індекс першого елемента рядка =", "?"); //! замініть "?" на вірне число
+
+
+
+//todo [3]
+console.log(
+  "%c [3] ",
+  "color: yellow; background-color: #2274A5",
+);
+//? Спочатку зробити дві змінні: 
+//? першу - <myFavoriteActor> або <myFavoriteActress> 
+//? і значенням = "Ім'я вашого улюбленного актора або актриси"
+//? наприклад, "Russell Crowe" або "Juliette Binoche"
+//? та другу - <male> або <female> зі значенням = "my favorite actor's name" 
+//? або "my favorite actress's name".
+//? Отримати ОСТАННІЙ символ (в обох прикладах це - "e") 
+//? змінної <myFavoriteActor> або <myFavoriteActress>
+//? та записати це значення у третю змінну <lastCharacter>.
+//? Зробити перетворення значення змінної <lastCharacter> 
+//? на ВЕЛИКУ(заглавну літеру) - "E" (буде в обох прикладах).
+//? Вивести в консоль повідомлення, використовуючи 
+//? значення всіх трьох змінних у такому форматі:
+//? "The last letter in my favorite actor's Russell Crowe is 'E'!" або
+//? "The last letter in favorite actress's Juliette Binoche is 'E'!".
+//! Код виконаного завдання
+
+
+
+//todo [4]
+console.log(
+  "%c [4] ",
+  "color: yellow; background-color: #2274A5",
+);
+//? Чи правильно змінюється рядок?
+let product = "Морозиво";
+// product[1] = "O"; //! так чи ні?
+console.log("product:", product);
+
+
+
+//todo [5]
+console.log(
+  "%c [5] ",
+  "color: yellow; background-color: #2274A5",
+);
+//? Зробити перетворення значення змінної
+//?  <myText3> у верхній регістр (на ВЕЛИКІ(заглавні літери))
+const myText = 'мій серіал';
+//! Код виконаного завдання
+
+
+
+//todo [6]
+console.log(
+  "%c [6] ",
+  "color: yellow; background-color: #2274A5",
+);
+//? Чи містить фраза "Meryl Streep" це слово "STREEP"?
+//? Здійсни перевірку незалежно від регістру
+//! Код виконаного завдання
+
+
+
+//todo [7]
+console.log(
+  "%c [7] ",
+  "color: yellow; background-color: #2274A5",
+);
+//? Що буде у консолі - true чи false?
+const myText5 = "стілець";
+console.log(myText5.includes("лец", 4)); //! false або true?
+//? Нащо треба змінити другий аргумент, 
+//? щоб у консолі було true?
+//! Код виконаного завдання
+
+
+
+//todo [8]
+console.log(
+  "%c [8] ",
+  "color: yellow; background-color: #2274A5",
+);
+//? Рядок "styles.css" треба перевірити 
+//? чи закінчується на ".css" чи на  ".js"?
+console.log("styles.css");
+//! Код виконаного завдання
