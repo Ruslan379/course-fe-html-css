e648cee73f255d5a7e9f8a872af6f05f31c8ac7c
Ruslan <arnov@i.ua>
2024-12-09 15:56:41 +0200

    Next 1446-JS

--- lesson-FE3_04/js/03-fe3-js_04.js
+++ lesson-FE3_04/js/03-fe3-js_04.js
@@ -4,143 +4,44 @@ console.log(
 );
 
 
-//! 3.Оператори розгалуження. 
-//! Інструкції: «if», «if...else», «else...if».
+//! Оператори розгалуження.
+//? Логічні оператори не можуть самостійно
+//? управляти потоком виконання програми.
+//? Lля цього використовуються розгалуження.
+//? Всі вони влаштовані за одним принципом —
+//? вхідні дані приводяться до булю(true або false) і,
+//? в залежності від результату цього значення,
+//? потік програми направляється в ту чи іншу гілку.
+
+
+//! 3-1.Інструкція: «if»
+//? 
 console.log(
-    "%c 3.Оператори розгалуження.                   \n Інструкції: «if», «if...else», «else...if». ",
+    "%c 3-1.Інструкція: «if» ",
     "color: yellow; background-color: #2274A5",
 );
-let a;
-let b;
-a == b   //? нестрога (несувора) рівність
-a === b  //? строга (сувора) рівність
-a != b   //? нестрога (несувора) нерівність
-a !== b  //? строга (сувора) нерівність
-
-a = 100;
-b = "100";
-console.log("a =", a, ";", "typeof a =", typeof a);
-console.log("b =", b, ";", "typeof b =", typeof b);
+console.warn("../images/01_instructions-if​.jpg");
+// console.log("a =", a, ";", "typeof a =", typeof a);
 console.log("-----------------------------");
 
-console.log("a == b:", a == b); //! true
-console.log("a === b:", a === b); //! false
-console.log("a != b:", a != b); //! false
-console.log("a !== b:", a !== b); //! true
 
 
-//? Оператори нестрогої (несуворої) рівності:
-console.log("ПРИКЛАДИ:");
-console.log("5 == 5:", 5 == 5); //! true 
-console.log("5 == 3:", 5 == 3); //! false 
-console.log("5 != 3:", 5 != 3); //! true
-console.log("5 != 5:", 5 != 5); //! false 
-//todo -  Погано, бо виконується неявне перетворення рядків
-//todo    і булевого значення до ЧИСЛА:
-console.log("------- неявне перетворення до ЧИСЛА -------");
-console.log('5 == 5:', 5 == 5); //! true
-console.log('5 == "5":', 5 == "5"); //! true 
-console.log('5 != "5":', 5 != "5"); //! false 
-//* Boolean
-console.log("1 == true:", 1 == true); //! true
-console.log('"1" == true:', "1" == true); //! true
-console.log("[1] == true:", [1] == true); //! true
-console.log("1 != true:", 1 != true); //! false
+//! 3-2.Інструкція: «if...else»
+//? 
+console.log(
+    "%c 3-2.Інструкція: «if...else» ",
+    "color: yellow; background-color: #2274A5",
+);
+// console.log("a =", a, ";", "typeof a =", typeof a);
+console.log("-----------------------------");
+
+//! 3-3.Інструкція: «else...if»
+//?
+console.log(
+    "%c 3-3.Інструкція: «else...if» ",
+    "color: yellow; background-color: #2274A5",
+);
+// console.log("a =", a, ";", "typeof a =", typeof a);
+console.log("-----------------------------");
 
 
-//? Оператори строгої (суворої) рівності:
-//todo -  Добре, перетворення типів не виконується:
-console.log("---- перетворення до ЧИСЛА не виконується ----");
-console.log("5 === 5:", 5 === 5); //! true 
-console.log('5 === "5":', 5 === "5"); //! false 
-console.log('5 !== "5":', 5 !== "5"); //! true
-//* Boolean
-console.log("1 === true:", 1 === true); //! false
-console.log("1 != true:", 1 !== true); //! true 
-
-
-
-
-
-
-//! ПРАКТИКА
-//todo [0]
-// let resultOne = "25" - 5;
-// console.log(resultOne);
-// console.log(typeof resultOne);
-
-// let resultTwo = 10 * "80";
-// console.log(resultTwo);
-// console.log(typeof resultTwo);
-
-//? Пріорітет операторів
-
-//? Різний пріорітет
-// let resultOne = 2 - 1 * 5;
-// console.log(resultOne);
-
-//? Скобки
-// let resultTwo = (2 - 1) * 5;
-// console.log(resultTwo);
-
-//? Однаковий пріорітет
-// let resultThree = 2 - 1 + 5;
-// console.log(resultThree);
-
-
-//todo [1]
-//? Доповни код, присвоївши змінній totalPrice вираз для підрахунку 
-//? загальної суми замовлення. Змінна pricePerItem зберігає ціну за одиницю товару,
-//?  а orderedQuantity - кількість одиниць товару в замовленні.
-
-//? const pricePerItem = 3500;
-//? const orderedQuantity = 4;
-
-//? const totalPrice = pricePerItem * orderedQuantity;
-
-
-//todo [2]
-//? Заміни у виразах стандартні математичні оператори на комбіновані оператори
-
-// let a = 5;
-// let b = 10;
-let c = 15;
-let d = 20;
-
-//? Change code below this line
-// a = a + 2;
-// b = b - 4;
-// c = c * 3;
-// d = d / 10;
-
-// a += 2;
-// b -= 4;
-// c *= 3;
-// d /= 10;
-
-
-//todo [3]
-//? Оголоси змінну isAdult та задай їй результат перевірки значення змінної age.
-//?  Використай оператори порівняння та доповни код так, щоб 
-//?  значенням isAdult було true, якщо вік користувача більше або дорівнює 18 
-//?  та false, якщо менше 18.
-// const age = 16;
-// const isAdult = age >= 18;
-
-
-//todo [4]
-//? Оголоси змінну isValid і задай їй значенням результат перевірки
-//?  рівності паролей в змінних correctPassword і userPassword.
-//? Якщо паролі співпадають (сувора рівність), значення isValid має бути true
-//? Якщо паролі не співпадають, значення isValid має бути false
-
-const correctPassword = "jqueryismyjam";
-const userPassword = "mangodab3st";
-const isValid = correctPassword === userPassword;
-
-// let a = 1 + 2;
-// let b = 2;
-
-// let result = 8 - (a = b + 3);
-// console.log("Результат в скобках: " + a);
-// console.log("Общий результат: " + result);
--- lesson-FE3_04/js/10-practice.js
+++ lesson-FE3_04/js/10-practice.js
@@ -11,11 +11,101 @@ console.log(
   "%c [1] ",
   "color: yellow; background-color: #2274A5",
 );
-//? 
-//? 
-//? 
-//? 
-//? 
-//! Код виконаного завдання
+// let resultOne = "25" - 5;
+// console.log(resultOne);
+// console.log(typeof resultOne);
+
+// let resultTwo = 10 * "80";
+// console.log(resultTwo);
+// console.log(typeof resultTwo);
+
+//? Пріорітет операторів
+
+//? Різний пріорітет
+// let resultOne = 2 - 1 * 5;
+// console.log(resultOne);
+
+//? Скобки
+// let resultTwo = (2 - 1) * 5;
+// console.log(resultTwo);
+
+//? Однаковий пріорітет
+// let resultThree = 2 - 1 + 5;
+// console.log(resultThree);
+
+
+//todo [2]
+console.log(
+  "%c [2] ",
+  "color: yellow; background-color: #2274A5",
+);
+//? Доповни код, присвоївши змінній totalPrice вираз для підрахунку
+//? загальної суми замовлення. Змінна pricePerItem зберігає ціну за одиницю товару,
+//?  а orderedQuantity - кількість одиниць товару в замовленні.
+
+// const pricePerItem = 3500;
+// const orderedQuantity = 4;
+
+// const totalPrice = pricePerItem * orderedQuantity;
+
+
+//todo [3]
+console.log(
+  "%c [3] ",
+  "color: yellow; background-color: #2274A5",
+);
+//? Заміни у виразах стандартні математичні оператори на комбіновані оператори
+
+// let a = 5;
+// let b = 10;
+let c = 15;
+let d = 20;
+
+//? Change code below this line
+// a = a + 2;
+// b = b - 4;
+// c = c * 3;
+// d = d / 10;
+
+// a += 2;
+// b -= 4;
+// c *= 3;
+// d /= 10;
+
+
+//todo [4]
+console.log(
+  "%c [4] ",
+  "color: yellow; background-color: #2274A5",
+);
+//? Оголоси змінну isAdult та задай їй результат перевірки значення змінної age.
+//?  Використай оператори порівняння та доповни код так, щоб
+//?  значенням isAdult було true, якщо вік користувача більше або дорівнює 18
+//?  та false, якщо менше 18.
+// const age = 16;
+// const isAdult = age >= 18;
+
+
+//todo [5]
+console.log(
+  "%c [5] ",
+  "color: yellow; background-color: #2274A5",
+);
+//? Оголоси змінну isValid і задай їй значенням результат перевірки
+//? рівності паролей в змінних correctPassword і userPassword.
+//? Якщо паролі співпадають (сувора рівність), значення isValid має бути true
+//? Якщо паролі не співпадають, значення isValid має бути false
+
+const correctPassword = "jqueryismyjam";
+const userPassword = "mangodab3st";
+const isValid = correctPassword === userPassword;
+
+// let a = 1 + 2;
+// let b = 2;
+
+// let result = 8 - (a = b + 3);
+// console.log("Результат в скобках: " + a);
+// console.log("Общий результат: " + result);
+
 
 
