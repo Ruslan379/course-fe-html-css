da64e637b39c07521f468204f652ca05cea931c1
Ruslan379 <arnov@i.ua>
2024-12-09 12:20:15 +0200

    Next 1438-JS

--- lesson-FE3_04/js/02-fe3-js_04.js
+++ lesson-FE3_04/js/02-fe3-js_04.js
@@ -3,17 +3,20 @@ console.log(
     'color: white; background-color: #D33F49',
 );
 
-//! Логічні оператори
+//! 2.Логічні оператори
 //? ​Існує три логічні оператори, які використовуються 
 //? для перевірки виконання множинних виразів:
-//? «І»-&&, «АБО»-||, «НЕ»-!.
-//todo Результатом роботи Логічних операторів є: true або false
+//? «І»(оператор &&), «АБО»(оператор ||), «НЕ»(оператор !).
 
 
-//! Логічне І (оператор &&)
-//* вираз && вираз
-//? Запинається на брехні
-//? Повертає (false або false), те на чому запнулось або останній операнд.
+//! 2-1.Логічне «І» (оператор &&)
+console.log(
+    "%c 2-1.Логічне «І» (оператор &&) ",
+    "color: yellow; background-color: #2274A5",
+);
+//todo: вираз && вираз
+//? ❗️❗️❗️ Запинається на БРЕХНІ.
+//* Повертає те на чому запнулось або останній операнд.
 //? Оператор && приводить всі операнди до булю і повертає одне зі значень.
 //? Щоб оператор && повернув true, потрібно щоб 
 //? всі операнди були істинними (truthy)
@@ -21,14 +24,54 @@ console.log("false && false:", false && false); //! false
 console.log("false && true:", false && true); //! false
 console.log("true && false:", true && false); //! false
 console.log("true && true:", true && true); //! true
-console.log("----------------------");
+console.log("----------------------------");
 
 //? Приклади:
-console.log('1 && 4 && 0 && "mango"', 1 && 4 && 0 && "mango"); //! false
+console.log('1 && 4:', 1 && 4); //! 4
+console.log('1 && 4 && 0:', 1 && 4 && 0); //! 0
+console.log('1 && 4 && "mango":', 1 && 4 && "mango"); //! mango
+console.log('1 && 4 && 0 && "mango":', 1 && 4 && 0 && "mango"); //! 0
+console.log('1 && 4 && "":', 1 && 4 && ""); //! пустий рядок 
+console.log('1 && "" && 4:', 1 && "" && 4); //! також пустий рядок
+console.log("----------------------------");
 
-const num = 20;
+let num = 20;
 const result1 = num > 10 && num < 30;
 console.log("num > 10 && num < 30:", result1); //! true
 
 const result2 = num > 10 && num < 10;
 console.log("num > 10 && num < 10:", result2); //! true
+
+
+//! 2-2.Логічне «АБО» (оператор ||)
+console.log(
+    "%c 2-2.Логічне «АБО» (оператор ||) ",
+    "color: yellow; background-color: #2274A5",
+);
+//todo: вираз || вираз
+//? ❗️❗️❗️ Запинається на ПРАВДІ.
+//* Повертає те на чому запнулось або останній операнд.
+//? Оператор && приводить всі операнди до булю і повертає одне зі значень.
+//? Щоб оператор && повернув true, достат щоб 
+//? всі операнди були істинними (truthy)
+console.log("false && false:", false && false); //! false
+console.log("false && true:", false && true); //! false
+console.log("true && false:", true && false); //! false
+console.log("true && true:", true && true); //! true
+console.log("----------------------------");
+
+//? Приклади:
+console.log('1 && 4:', 1 && 4); //! 4
+console.log('1 && 4 && 0:', 1 && 4 && 0); //! 0
+console.log('1 && 4 && "mango":', 1 && 4 && "mango"); //! mango
+console.log('1 && 4 && 0 && "mango":', 1 && 4 && 0 && "mango"); //! 0
+console.log('1 && 4 && "":', 1 && 4 && ""); //! пустий рядок 
+console.log('1 && "" && 4:', 1 && "" && 4); //! також пустий рядок
+console.log("----------------------------");
+
+num = 20;
+const result3 = num > 10 && num < 30;
+console.log("num > 10 && num < 30:", result3); //! true
+
+const result4 = num > 10 && num < 10;
+console.log("num > 10 && num < 10:", result4); //! true
