b0894a3370067a2d8de7fec539c195b05ecd2e69
Ruslan379 <arnov@i.ua>
2024-12-09 12:25:29 +0200

    Next 1439-JS

--- lesson-FE3_04/js/02-fe3-js_04.js
+++ lesson-FE3_04/js/02-fe3-js_04.js
@@ -51,27 +51,27 @@ console.log(
 //todo: вираз || вираз
 //? ❗️❗️❗️ Запинається на ПРАВДІ.
 //* Повертає те на чому запнулось або останній операнд.
-//? Оператор && приводить всі операнди до булю і повертає одне зі значень.
-//? Щоб оператор && повернув true, достат щоб 
-//? всі операнди були істинними (truthy)
-console.log("false && false:", false && false); //! false
-console.log("false && true:", false && true); //! false
-console.log("true && false:", true && false); //! false
-console.log("true && true:", true && true); //! true
+//? Оператор || приводить всі операнди до булю і повертає одне зі значень.
+//? Щоб оператор || повернув true, достатньо щоб 
+//? хоч би один операнд був істинним (truthy)
+console.log("false || false:", false || false); //! false
+console.log("false || true:", false || true); //! false
+console.log("true || false:", true || false); //! false
+console.log("true || true:", true || true); //! true
 console.log("----------------------------");
 
 //? Приклади:
-console.log('1 && 4:', 1 && 4); //! 4
-console.log('1 && 4 && 0:', 1 && 4 && 0); //! 0
-console.log('1 && 4 && "mango":', 1 && 4 && "mango"); //! mango
-console.log('1 && 4 && 0 && "mango":', 1 && 4 && 0 && "mango"); //! 0
-console.log('1 && 4 && "":', 1 && 4 && ""); //! пустий рядок 
-console.log('1 && "" && 4:', 1 && "" && 4); //! також пустий рядок
+console.log('1 || 4:', 1 || 4); //! 4
+console.log('1 || 4 || 0:', 1 || 4 || 0); //! 0
+console.log('1 || 4 || "mango":', 1 || 4 || "mango"); //! mango
+console.log('1 || 4 || 0 || "mango":', 1 || 4 || 0 || "mango"); //! 0
+console.log('1 || 4 || "":', 1 || 4 || ""); //! пустий рядок 
+console.log('1 || "" || 4:', 1 || "" || 4); //! також пустий рядок
 console.log("----------------------------");
 
 num = 20;
-const result3 = num > 10 && num < 30;
-console.log("num > 10 && num < 30:", result3); //! true
+const result3 = num > 10 || num < 30;
+console.log("num > 10 || num < 30:", result3); //! true
 
-const result4 = num > 10 && num < 10;
-console.log("num > 10 && num < 10:", result4); //! true
+const result4 = num > 10 || num < 10;
+console.log("num > 10 || num < 10:", result4); //! true
