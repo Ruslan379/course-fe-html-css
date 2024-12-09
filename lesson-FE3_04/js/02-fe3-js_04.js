3cb11b7cd743a53d262966a1cbdf45599130f2d2
Ruslan379 <arnov@i.ua>
2024-12-09 11:37:01 +0200

    Next 1437-JS

--- lesson-FE3_04/js/02-fe3-js_04.js
+++ lesson-FE3_04/js/02-fe3-js_04.js
@@ -3,21 +3,32 @@ console.log(
     'color: white; background-color: #D33F49',
 );
 
-//! Оператори порівняння
-//? Використовуються для порівняння двох значень і 
-//? повертають булеве значення (true або false)
+//! Логічні оператори
+//? ​Існує три логічні оператори, які використовуються 
+//? для перевірки виконання множинних виразів:
+//? «І»-&&, «АБО»-||, «НЕ»-!.
+//todo Результатом роботи Логічних операторів є: true або false
 
-const a = 20;
-const b = 50;
-console.log("a =", a);
-console.log("b =", b);
-console.log("--------------");
 
-console.log("a > b:", a > b); //! false
-console.log("b > a:", b > a); //! true
-console.log("a >= b:", a >= b); //! false
-console.log("b >= a:", b >= a); //! true
-console.log("a < b:", a < b); //! true
-console.log("b < a:", b < a); //! false
-console.log("a <= b:", a <= b); //! true
-console.log("b <= a:", b <= a); //! false
+//! Логічне І (оператор &&)
+//* вираз && вираз
+//? Запинається на брехні
+//? Повертає (false або false), те на чому запнулось або останній операнд.
+//? Оператор && приводить всі операнди до булю і повертає одне зі значень.
+//? Щоб оператор && повернув true, потрібно щоб 
+//? всі операнди були істинними (truthy)
+console.log("false && false:", false && false); //! false
+console.log("false && true:", false && true); //! false
+console.log("true && false:", true && false); //! false
+console.log("true && true:", true && true); //! true
+console.log("----------------------");
+
+//? Приклади:
+console.log('1 && 4 && 0 && "mango"', 1 && 4 && 0 && "mango"); //! false
+
+const num = 20;
+const result1 = num > 10 && num < 30;
+console.log("num > 10 && num < 30:", result1); //! true
+
+const result2 = num > 10 && num < 10;
+console.log("num > 10 && num < 10:", result2); //! true
