f74d079ebb0159758201a7e90bce95987d6f6f56
Ruslan <arnov@i.ua>
2024-12-22 14:02:09 +0200

    Next 1484-JS

--- lesson-FE3_06/js/01-fe3-js_06.js
+++ lesson-FE3_06/js/01-fe3-js_06.js
@@ -9,3 +9,12 @@ console.log(
 console.warn("https://ruslan379.github.io/course-fe-html-css/lesson-FE3_06/images/while.png");
 
 // console.log("cost:", cost); //! 100
+
+
+console.log('isNaN(" "):', isNaN(" ")); //! false
+console.log('isNaN(true):', isNaN(true)); //! false
+console.log('isNaN(null):', isNaN(null)); //! false
+
+console.log('parseFloat(null):', parseFloat(null)); //! NaN
+console.log('parseFloat(" "):', parseFloat(" ")); //! NaN
+console.log('parseFloat(true):', parseFloat(true)); //! NaN
