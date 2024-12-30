87a4708a7ba1b2e0e34ecc0dd30615f26d9aae97
Ruslan <arnov@i.ua>
2024-12-30 15:46:39 +0200

    Next 1503-JS

--- lesson-FE3_07/js/02-fe3-js_07.js
+++ lesson-FE3_07/js/02-fe3-js_07.js
@@ -74,14 +74,17 @@ console.warn("Оператори break і continue​​:");
 //todo: ПРИКЛАД-1 (var.1)
 console.warn("ПРИКЛАД-1 (var.1):");
 const clients1 = ["Mango", "Poly", "Ajax"];
-const clientNameToFind = "Poly";
+console.log("clients1:", clients1); //! ['Mango', 'Poly', 'Ajax']
+const clientNameToFind1 = "Poly";
+console.log("clientNameToFind1:", clientNameToFind1); //! 'Poly'
+
 let message;
 
 for (const client of clients1) {
     //? На кожній ітерації будемо перевіряти чи збігається елемент масиву з
     //? іменем клієнта. Якщо збігається - записуємо в message повідомлення
     //? про успіх і робимо break, щоб далі не шукати
-    if (client === clientNameToFind) {
+    if (client === clientNameToFind1) {
         message = "Клієнт з таким ім'ям є в базі даних!";
         break;
     };
@@ -101,7 +104,9 @@ console.warn("ПРИКЛАД-1 (var.2):");
 //? а потрібний нам знаходиться на 2 позиції, 
 //? то немає абсолютно жодного сенсу перебирати інші 9998 елементи.
 const clients2 = ["Mango", "Poly", "Ajax"];
+console.log("clients2:", clients2); //! ['Mango', 'Poly', 'Ajax']
 const clientNameToFind2 = "Ajax";
+console.log("clientNameToFind2:", clientNameToFind2); //! 'Ajax'
 let message2 = "Клієнт з таким ім'ям відсутній в базі даних!";
 
 for (const client of clients2) {
@@ -121,7 +126,9 @@ console.warn("ПРИКЛАД-2:");
 //? Використовуємо цикл для виведення
 //? тільки чисел, більших за певне значення.
 const numbers = [1, 3, 14, 18, 4, 7, 29, 6, 34];
+console.log("Масив чисел:", numbers); //! [1, 3, 14, 18, 4, 7, 29, 6, 34]
 const threshold = 15;
+console.log("Число для порівняння:", threshold); //! 15
 
 //? Для чисел, менших ніж порогове значення, спрацьовує continue, виконання тіла
 //? припиняється і управління передається на наступну ітерацію.
