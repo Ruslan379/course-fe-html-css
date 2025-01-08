ede6043b86aeeffd3d2047223874d53bccafd417
Ruslan <arnov@i.ua>
2025-01-08 17:05:33 +0200

    Next 1538-JS

--- lesson-FE3_08/js/10-practice.js
+++ lesson-FE3_08/js/10-practice.js
@@ -14,12 +14,11 @@ console.log(
 const arrayOriginal = ["мango-10", "Kiwi-2", "Monkong-2", "Singu-2", "Mango-1", "Ajax-1", "Poly-1", "singu-20"];
 //? на масив arrayNew:
 //todo: ["Mango-1", "Ajax-1", "Poly-1", "Kiwi-2", "Monkong-2", "Singu-2"];
-//? не змінюючи значення єлементів та використовуючи методи:
+//? не змінюючи значення елементів та використовуючи методи:
 //? push(), pop(), unshift(), shift(),
 //? slice(), splice(), concat() за власним бажанням!
 //! Код виконаного завдання
-const arrayNew = [];
+let arrayNew = [];
 console.log("arrayOriginal:", arrayOriginal);
 console.log("arrayNew:", arrayNew);
 console.log(". . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .");
-
