94fcec9aef9c0ed6be858966eef8425021385f13
Ruslan <103874267+Ruslan379@users.noreply.github.com>
2025-01-08 11:13:51 +0200

    Next 1526-JS

--- lesson-FE3_08/js/05-fe3-js_08.js
+++ lesson-FE3_08/js/05-fe3-js_08.js
@@ -4,6 +4,15 @@ console.log(
 );
 
 //! 5.Метод slice()
-//? 
+console.warn("Метод slice(): \n https://ruslan379.github.io/course-fe-html-css/lesson-FE3_08/images/slice.png");
+//? .slice(start, end)  - ПОВЕРТАЄ новий масив, що містить
+//? КОПІЮ частини початкового масиву, не змінюючи його.
+//? КОПІЯ нового масиву створюється ВІД start і ДО end, але НЕ ВКЛЮЧНО end.
+//? start та end - індекси елементів початкового масиву.
+const clients = ["Mango", "Ajax", "Poly", "Kiwi"];
+console.log("clients:", clients); //!
 
+const clientsSlice = clients.slice(1, 3);
+console.log("clients.slice(1, 3):", clientsSlice); //!
 console.log("-----------------------------------------");
+
