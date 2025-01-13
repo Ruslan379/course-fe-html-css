0976171876733d35e48d10d0d120a62decd1f231
Ruslan <arnov@i.ua>
2025-01-13 16:16:58 +0200

    Next 1551-JS

--- lesson-FE3_09/js/10-practice.js
+++ lesson-FE3_09/js/10-practice.js
@@ -4,21 +4,11 @@ console.log(
   "color: white; background-color: #D33F49",
 );
 
-//! ПРАКТИКА-8 (Урок-08_JS)
+//! ПРАКТИКА-9 (Урок-09_JS)
 //todo [1]
 console.log(
   "%c [1] ",
   "color: yellow; background-color: #2274A5",
 );
-//? Перетворити масив:
-const arrayOriginal = ["мango-10", "Kiwi-2", "Monkong-2", "Singu-2", "Mango-1", "Ajax-1", "Poly-1", "singu-20"];
-//? на масив arrayNew:
-//todo: ["Mango-1", "Ajax-1", "Poly-1", "Kiwi-2", "Monkong-2", "Singu-2"];
-//? не змінюючи значення елементів та використовуючи методи:
-//? push(), pop(), unshift(), shift(),
-//? slice(), splice(), concat() за власним бажанням!
-//! Код виконаного завдання
-let arrayNew = [];
-console.log("arrayOriginal:", arrayOriginal);
-console.log("arrayNew:", arrayNew);
+//? 
 console.log(". . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .");
