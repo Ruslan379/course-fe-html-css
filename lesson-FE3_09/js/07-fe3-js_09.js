cb1b1273b072dccdb7d43d76683f97a4e783c47f
Ruslan <arnov@i.ua>
2025-01-15 10:56:18 +0200

    Next 1559-JS

--- lesson-FE3_09/js/07-fe3-js_09.js
+++ lesson-FE3_09/js/07-fe3-js_09.js
@@ -1,10 +1,10 @@
 console.log(
-    '%c 7.Hoisting ',
+    '%c 7.Hoisting (Підняття змінних) ',
     'color: white; background-color: #D33F49',
 );
 
-//! 7.Hoisting
-console.warn("Hoisting:");
+//! 7.Hoisting (Підняття змінних)
+// console.warn("7.Hoisting (Підняття змінних):");
 //? 
 
 console.log("-----------------------------------------------------------");
