f03b2a24e5c8dd4c7d9685275a2c0fe3b44bc26f
Ruslan <arnov@i.ua>
2025-01-15 19:54:51 +0200

    Next 1567-JS

--- lesson-FE3_09/js/05-fe3-js_09.js
+++ lesson-FE3_09/js/05-fe3-js_09.js
@@ -12,11 +12,11 @@ console.warn("Створення ти виклики функції (function de
 //? що функцію можна викликати як
 //? ✅ ДО її оголошення(створення) в коді, так i
 //? ✅ ПІСЛЯ її оголошення(створення).
-fnDec(); //todo✅: виклик  функції (function declaration) ДО її створення
-function fnDec() {
-    console.log("Це функція 'fnDec'");
+fnDeclaration(); //todo✅: виклик  функції (function declaration) ДО її створення
+function fnDeclaration() {
+    console.log("Це функція 'fnDeclaration'");
 };
-fnDec(); //todo✅: виклик  функції (function declaration) ПІСЛЯ її створення
+fnDeclaration(); //todo✅: виклик  функції (function declaration) ПІСЛЯ її створення
 console.log(". . . . . . . . . . . . . . . . . . . . . . . . . . . ");
 
 console.warn("Створення ти виклики функції (function expression):");
@@ -24,9 +24,9 @@ console.warn("Створення ти виклики функції (function ex
 //? ❌ не можна викликати ДО місця його оголошення(створення),
 //? ✅ тільки ПІСЛЯ місця його оголошення(створення),
 //?  тому що це буквально оголошення const змінної.
-// fnExp(); //todo❌: виклик  функції (function expression) ДО її створення
-const fnExp = function() {
-    console.log("Це функція 'fnExp'");
+// fnExpression(); //todo❌: виклик  функції (function expression) ДО її створення
+const fnExpression = function() {
+    console.log("Це функція 'fnExpression'");
 };
-fnExp(); //todo✅: виклик  функції (function expression) ПІСЛЯ її створення
+fnExpression(); //todo✅: виклик  функції (function expression) ПІСЛЯ її створення
 console.log("-----------------------------------------------------");
