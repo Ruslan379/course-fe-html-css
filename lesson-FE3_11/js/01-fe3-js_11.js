472745a17f530719ec997cdac2a9da1bfe8eabc2
Ruslan <arnov@i.ua>
2025-01-27 14:42:31 +0200

    Next 1600-JS

--- lesson-FE3_11/js/01-fe3-js_11.js
+++ lesson-FE3_11/js/01-fe3-js_11.js
@@ -15,22 +15,70 @@ console.log(
 
 //! Створення об'єкта
 console.warn("Створення об'єкта:");
-//? Для оголошення використовуються фігурні дужки {} - літерал об'єкта.
 
-//? Під час створення об'єкта можна додати властивості, 
-//? кожна з яких описується параметрами -  ключ: значення. 
-//? КЛЮЧ ще називають ІМ'ЯМ властивості і це завжди рядок. 
+//todo: Приклад створення масива:
+const array = [2, 21, 34, 89, 144];
+
+for (let i = 0; i < array.length; i++) {
+    const element = array[i];
+    console.log(`${i}: ${element}`);
+};
+console.log(". . . . . . . . . . . . . . . . . . . . . .");
+
+//? Для оголошення об'єкта використовуються
+//? фігурні дужки {} - літерал об'єкта.
+//todo: Приклад створення об'єкта:
+let object = { 0: 2, 1: 21, 2: 34, 3: 89, 4: 144 };
+
+// for (const element of object) {
+//     console.log(element); //! ❌ TypeError: object is not iterable
+// };
+
+for (const element in object) {
+    console.log(`${element}: ${object[element]}`); //! ✅
+};
+
+object = {
+    0: 2,
+    1: 21,
+    2: 34,
+    3: 89,
+    4: 144
+};
+
+console.log("object:", object);
+console.log("typeof object:", typeof object); //! object
+console.log(". . . . . . . . . . . . . . . . . . . . . .");
+
+const object2 = {
+    перший: 2,
+    другий: 21,
+    третій: 34,
+    четвертий: 89,
+    "п'ятий": 144,
+    // перший: 6765,
+};
+
+console.log("object2:", object2);
+console.log(". . . . . . . . . . . . . . . . . . . . . .");
+
+
+//? Під час створення об'єкта можна додати властивості,
+//? кожна з яких описується параметрами -  КЛЮЧ: ЗНАЧЕННЯ. 
+//? КЛЮЧ ще називають ІМ'ЯМ властивості і це завжди рядок! 
 //? ЗНАЧЕННЯМ властивості можуть бути будь-які типи: 
 //? примітиви, масиви, об'єкти, булі, функції тощо. 
 //? Властивості розділяються комою.
-const book = {
+const bookObject = {
     title: "The Last Kingdom",
     author: "Bernard Cornwell",
     genres: ["historical prose", "adventure"],
     isPublic: true,
     rating: 8.38,
 };
-console.log("book:", book); //! true
+console.log("bookObject:", bookObject);
+console.log("typeof bookObject:", typeof bookObject); //! object
+console.log("typeof null:", typeof null); //! object
 console.log("---------------------------------------------------------------------------");
 
 
