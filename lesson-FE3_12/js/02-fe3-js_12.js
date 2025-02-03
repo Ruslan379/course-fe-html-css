adf89f4ab64229fc2a146ede98942abee094e9e7
Ruslan <arnov@i.ua>
2025-02-03 17:52:38 +0200

    Next 1625-JS

--- lesson-FE3_12/js/02-fe3-js_12.js
+++ lesson-FE3_12/js/02-fe3-js_12.js
@@ -4,9 +4,55 @@ console.log(
 );
 
 //! Методи об'єкта Object
-//? ✴️ 
+//? ✳️ Всі об'єкти походять від конструктора Object.
+//? У нього є кілька дуже корисних методів для роботи з об'єктами.
 
-//? ✳️ 
+//! Object.assign
+//? ✴️ Object.assign - додає один
+//? або декілька методів до об'єкту.
+const bookShelf = {
+    books: ["The Last Kingdom"],
+    // getBooks() {
+    //     return this.books;
+    // },
+    // addBook(bookName) {
+    //     this.books.push(bookName);
+    // },
+    // removeBook(bookName) {
+    //     const bookIndex = this.books.indexOf(bookName);
+    //     this.books.splice(bookIndex, 1);
+    // },
+};
+console.log("books_before:", bookShelf); 
+
+
+//todo: ✴️ var.1
+//todo: Додавання до об'єкту метода "getBooks":
+bookShelf.getBooks = function (money) {
+    return this.books;
+};
+
+//todo: Додавання до об'єкту метода "addBook":
+bookShelf.addBook = function (bookName) {
+    this.books.push(bookName)
+};
+
+//todo: Додавання до об'єкту метода "removeBook":
+bookShelf.removeBook = function (bookName) {
+    const bookIndex = this.books.indexOf(bookName);
+    this.books.splice(bookIndex, 1);
+};
+console.log("books_after:", bookShelf); 
+
+
+//todo: ✳️ var.2
+
+bookShelf.addBook("The Mist");
+bookShelf.addBook("Dream Guardian");
+console.log("books_add:", bookShelf.getBooks()); //! ['The Last Kingdom', 'The Mist', 'Dream Guardian']
+
+bookShelf.removeBook("The Mist");
+console.log("books_remove:", bookShelf.getBooks()); //! ['The Last Kingdom', 'Dream Guardian']
 
 
 console.log(". . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .");
