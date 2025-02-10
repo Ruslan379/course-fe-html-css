2b6851af018d60b3fa36899b4a290d47b206cd87
Ruslan <arnov@i.ua>
2025-02-10 17:45:48 +0200

    Next 1661-JS

--- lesson-FE3_13/js/01-fe3-js_13.js
+++ lesson-FE3_13/js/01-fe3-js_13.js
@@ -107,3 +107,130 @@ console.log("isPublic1:", isPublic1);
 console.log("rating1:", rating1);
 console.log("coverImage1:", coverImage1); //! "https://via.placeholder.com/640/480"
 console.log("----------------------------------------------------------------------------------------------------------------------");
+
+
+//! Зміна імені змінної
+console.warn("Зміна імені змінної:");
+//? ✴️ Під час деструктуризації можна змінити ім'я змінної, 
+//? в яку розпаковується значення властивості. 
+//? Спочатку пишемо ім'я властивості, з якої хочемо отримати значення, 
+//? після чого ставимо двокрапку і пишемо ім'я змінної, 
+//? в яку необхідно помістити значення цієї властивості.
+const firstBook = {
+    title: "The Last Kingdom",
+    coverImage: "https://images-na.ssl-images-amazon.com/images/I/51b5YG6Y1rL.jpg",
+};
+console.log("firstBook:", firstBook);
+console.log("`  `  `  `  `  `  `  `  `  `  `  `  `  `  `  `  `  `  `  `  `  `  `  `  `  `  `  `  `  `  `  `  `  `  `  `  `  `");
+
+const {
+    title: firstTitle,
+    coverImage: firstCoverImage = "https://via.placeholder.com/640/480",
+} = firstBook;
+
+console.log("firstTitle:", firstTitle); //! The Last Kingdom
+console.log("firstCoverImage:", firstCoverImage);  //! https://images-na.ssl-images-amazon.com/images/I/51b5YG6Y1rL.jpg
+console.log(". . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .");
+
+const secondBook = {
+    title: "Сон смішної людини",
+};
+console.log("secondBook:", secondBook);
+console.log("`  `  `  `  `  `  `  `  `  `  `  `  `  `  `  `  `  `  `  `  `  `  `  `  `  `  `  `  `  `  `  `  `  `  `  `  `  `");
+
+const {
+    title: secondTitle,
+    coverImage: secondCoverImage = "https://via.placeholder.com/640/480",
+} = secondBook;
+
+console.log("secondTitle:", secondTitle);  //! Сон смішної людини
+console.log("secondCoverImage:", secondCoverImage); //! https://via.placeholder.com/640/480
+console.log("----------------------------------------------------------------------------------------------------------------------");
+
+
+//! Деструктуризація в циклах
+console.warn("Деструктуризація в циклах:");
+//? ✴️ Перебираючи масив об'єктів циклом for...of,
+//? утворюються багаторазові звернення до властивостей об'єкта.
+console.warn("Приклад-1:");
+const books = [
+    {
+        title: "The Last Kingdom",
+        author: "Bernard Cornwell",
+        rating: 8.38,
+    },
+    {
+        title: "На березі спокійних вод",
+        author: "Роберт Шеклі",
+        rating: 8.51,
+    },
+];
+
+for (const book of books) {
+    console.log("book.title:", book.title);
+    console.log("book.author:", book.author);
+    console.log("book.rating:", book.rating);
+    console.log("`  `  `  `  `  `  `  `  `  `  `  `  `");
+};
+console.log(". . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .");
+
+
+console.warn("Приклад-2:");
+//? ✴️ Для того, щоб скоротити кількість повторень, 
+//? можна деструктуризувати властивості об'єкта 
+//? у локальні змінні в тілі циклу.
+for (const book of books) {
+    const { title, author, rating } = book;
+
+    console.log("title:", title);
+    console.log("author:", author);
+    console.log("rating:", rating);
+    console.log("`  `  `  `  `  `  `  `  `  `  `  `  `");
+};
+console.log(". . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .");
+
+
+console.warn("Приклад-3:");
+//? ✴️ Якщо в об'єкті небагато властивостей,
+//? можна виконати деструктуризацію
+//? безпосередньо у місці оголошення змінної book.
+for (const { title, author, rating } of books) {
+    console.log("title:", title);
+    console.log("author:", author);
+    console.log("rating:", rating);
+    console.log("`  `  `  `  `  `  `  `  `  `  `  `  `");
+};
+console.log("----------------------------------------------------------------------------------------------------------------------");
+
+
+//! Глибока деструктуризація
+console.warn("Глибока деструктуризація:");
+//? ✴️ Для деструктуризації властивостей
+//? вкладених об'єктів використовуються
+//? ті самі принципи, що й в трьох попередніх вправах.
+const user = {
+    name: "Jacques Gluke",
+    tag: "jgluke",
+    stats: {
+        followers: 5603,
+        views: 4827,
+        likes: 1308,
+    },
+};
+console.log("user:", user);
+console.log("`  `  `  `  `  `  `  `  `  `  `  `  `  `  `  `  `  `  `  `  `");
+
+const {
+    name,
+    tag,
+    stats,
+    stats: { followers, views: userViews, likes: userLikes = 0 },
+} = user;
+
+console.log("name:", name); //! Jacques Gluke
+console.log("tag:", tag); //! jgluke
+console.log("stats:", stats); //! {followers: 5603, views: 4827, likes: 1308}  або Uncaught ReferenceError: stats is not defined
+console.log("followers:", followers); //! 5603
+console.log("userViews:", userViews); //! 4827
+console.log("userLikes:", userLikes); //! 1308
+console.log("----------------------------------------------------------------------------------------------------------------------");
