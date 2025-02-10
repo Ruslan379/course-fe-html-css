6b806e620787188ab1c267e2e1d2e9b121786802
Ruslan <arnov@i.ua>
2025-02-10 16:29:52 +0200

    Next 1658-JS

---
+++ lesson-FE3_13/js/10-practice.js
@@ -0,0 +1,18 @@
+// console.log('10.Практика:');
+console.log(
+  "%c 10.Практика: ",
+  "color: white; background-color: #D33F49",
+);
+
+//! ПРАКТИКА-13 (Урок-13_JS)
+//todo [1]
+console.log(
+  "%c [1] ",
+  "color: yellow; background-color: #2274A5",
+);
+//?
+//! Код виконаного завдання
+
+console.log("--------------------------------------------");
+
+
