7f2fe25809261b2ba6ac1caee2a8f26667ad65ce
Ruslan <arnov@i.ua>
2025-02-11 17:07:16 +0200

    Next 1667-JS

--- lesson-FE3_13/js/10-practice.js
+++ lesson-FE3_13/js/10-practice.js
@@ -10,7 +10,64 @@ console.log(
   "%c [1] ",
   "color: yellow; background-color: #2274A5",
 );
-//?
+//? Деструктуризувати наступні об’єкти:
+// user
+const user = {
+  name: 'John',
+  age: 30,
+  email: 'john@example.com'
+};
+
+
+// car
+const car = {
+  make: 'Toyota',
+  model: 'Camry',
+  year: 2020,
+  features: ['power windows', 'rear camera', 'navigation'],
+  safety: {
+    airbags: true,
+    antilock_brakes: true,
+    stability_control: true
+  },
+};
+
+// movie
+const movie = {
+  title: 'The Shawshank Redemption',
+  director: {
+    name: 'Frank Darabont',
+    nationality: 'American'
+  },
+  actors: ['Tim Robbins', 'Morgan Freeman'],
+  release_year: 1994,
+  ratings: {
+    imdb: 9.3,
+    rotten_tomatoes: 90
+  },
+};
+
+// books
+const books = {
+  count: 3,
+  list: [
+    {
+      title: 'The Great Gatsby',
+      author: 'F. Scott Fitzgerald',
+      year: 1925
+    },
+    {
+      title: 'To Kill a Mockingbird',
+      author: 'Harper Lee',
+      year: 1960
+    },
+    {
+      title: '1984',
+      author: 'George Orwell',
+      year: 1949
+    }
+  ],
+};
 //! Код виконаного завдання
 
 console.log("--------------------------------------------");
