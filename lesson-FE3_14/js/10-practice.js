256c123b91de80a164e0175a03f2506de84f3b87
Ruslan <arnov@i.ua>
2025-02-19 15:22:27 +0200

    Next 1688-JS

--- lesson-FE3_14/js/10-practice.js
+++ lesson-FE3_14/js/10-practice.js
@@ -5,12 +5,52 @@ console.log(
 );
 
 //! ПРАКТИКА-14 (Урок-14_JS)
-//todo [1]
+//todo [Додаткове 14-1]
 console.log(
-  "%c [1] ",
+  "%c [Додаткове 14-1] ",
   "color: yellow; background-color: #2274A5",
 );
-//? 
-//! Код виконаного завдання
+//? Повернути масив користувачів, вік (збережений у властивості age) яких
+//? потрапляє у заданий діапазон від minAge до maxAge
+const getUsersWithAge = (users, minAge, maxAge) => {
+  //! твій код
 
-console.log("--------------------------------------------");
+};
+
+console.log(getUsersWithAge(users, 20, 30)); //! [об'єкт Ross Vazquez, об'єкт Elma Head, об'єкт Carey Barr]
+console.log(getUsersWithAge(users, 30, 40)); //! [об'єкт Moore Hensley, об'єкт Sharlene Bush, об'єкт Blackburn Dotson, об'єкт Sheree Anthony]
+console.log("--------------------------------------------------");
+
+
+
+//todo [Додаткове 14-2]
+console.log(
+  "%c [Додаткове 14-2] ",
+  "color: yellow; background-color: #2274A5",
+);
+//? З'ясувати, чи всі користувачі зараз активні (властивість isActive)
+//? та повернути true, якщо так або false, якщо ні.
+const isEveryUserActive = (users) => {
+  //! твій код
+
+};
+
+console.log("Чи всі користувачі зараз активні? -", isEveryUserActive(users)); //! false
+console.log("--------------------------------------------------");
+
+
+
+//todo [Додаткове 14-3]
+console.log(
+  "%c [Додаткове 14-3] ",
+  "color: yellow; background-color: #2274A5",
+);
+//? З'ясувати, чи є хоча б однин активний користувач (властивість isActive)
+//? та повернути true, якщо так або false, якщо ні.
+const isAnyUserActive = (users) => {
+  //! твій код
+
+};
+
+console.log("Чи є хоча б однин активний користувач? -", isAnyUserActive(users)); //! true
+console.log("--------------------------------------------------");
