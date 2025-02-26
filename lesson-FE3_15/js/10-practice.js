ec7f3a97ef0cebbcff8b146859a574cc552422bf
Ruslan <arnov@i.ua>
2025-02-26 17:39:56 +0200

    Next 1701-JS

--- lesson-FE3_15/js/10-practice.js
+++ lesson-FE3_15/js/10-practice.js
@@ -10,7 +10,15 @@ console.log(
   "%c [Додаткове 15-1] ",
   "color: yellow; background-color: #2274A5",
 );
-//? 
 
+const players = [
+  { id: 'player-1', name: 'Mango', timePlayed: 310, points: 54, online: false },
+  { id: 'player-2', name: 'Poly', timePlayed: 470, points: 92, online: true },
+  { id: 'player-3', name: 'Kiwi', timePlayed: 230, points: 48, online: true },
+  { id: 'player-4', name: 'Ajax', timePlayed: 150, points: 71, online: false },
+  { id: 'player-5', name: 'Chelsy', timePlayed: 80, points: 48, online: true },
+];
+//? Порахувати загальну кількість годин
+//? Відсортувати по кількості проведенного часу в грі
 console.log("--------------------------------------------------");
 
