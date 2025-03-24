52f9998a0a0b2d0104f7f7aeb6d2a7d171c22895
Ruslan <arnov@i.ua>
2025-03-24 11:52:59 +0200

    Next 1765-JS

--- lesson-FE3_19/js/10-practice.js
+++ lesson-FE3_19/js/10-practice.js
@@ -4,10 +4,10 @@ console.log(
   "color: white; background-color: #D33F49",
 );
 
-//! ПРАКТИКА-18 (Урок-18_JS)
-//todo [Додаткове 18-1]
+//! ПРАКТИКА-19 (Урок-19_JS)
+//todo [Додаткове 19-1]
 console.log(
-  "%c [Додаткове 18-1] ",
+  "%c [Додаткове 19-1] ",
   "color: yellow; background-color: #2274A5",
 );
 
