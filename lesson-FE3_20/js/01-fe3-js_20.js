1476d0cbac29a298e992344e0e3b87b4edc833b0
Ruslan <arnov@i.ua>
2025-03-30 20:40:14 +0300

    Next 1784-JS

--- lesson-FE3_20/js/01-fe3-js_20.js
+++ lesson-FE3_20/js/01-fe3-js_20.js
@@ -101,8 +101,7 @@ console.warn("Клавіші-модифікатори​:");
 document.addEventListener("keydown", event => {
     event.preventDefault();
     if ((event.ctrlKey || event.metaKey) && event.code === "KeyS") {
-        console.log("«Ctrl + s» or «Command + s» combo");
+        console.warn("«Ctrl + s» or «Command + s» combo");
     }
 });
-
 console.log("-----------------------------------------------------------------------------------------------------");
