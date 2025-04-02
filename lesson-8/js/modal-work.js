8cc26817a1c1e85f0e9c0115642233165dfb0eac
Ruslan <arnov@i.ua>
2025-04-02 20:55:13 +0300

    Next 1792-JS

--- lesson-8/js/modal-work.js
+++ lesson-8/js/modal-work.js
@@ -1,5 +1,5 @@
 //! Бєк-дроп з модальним вікном
-(() => {
+// (() => {
     const refs = {
         openModalBtn: document.querySelector("[data-modal-open]"),
         closeModalBtn: document.querySelector("[data-modal-close]"),
@@ -41,7 +41,8 @@
         };
         isOnlyBackdrop = true;
     }
-    
+
+
     //! Клік в модальне вікно
     refs.modal.addEventListener("click", logModalClick);
     
@@ -52,7 +53,7 @@
         };
     }
 
-    
+
     //! Додаткові налаштування форми
     function handleFormSubmit(event) {
         //! Запобігаємо перезавантаженню сторінки після надсилання форми
@@ -68,46 +69,14 @@
         refs.modalThanks.classList.toggle("is-hidden");
     }
 
-})();
 
-//! Додатковий функціонал – закриття модального вікна при натисканні клавіші <Escape>
-document.addEventListener("keydown", logKeyEscapeClick)
+    //! Додатковий функціонал – закриття модального вікна при натисканні клавіші <Escape>
+    document.addEventListener("keydown", logKeyEscapeClick)
 
-function logKeyEscapeClick(event) {
-    if (event.code === "Escape") {
-        // console.log("Натиснута клавіша <Escape>");
-        toggleModal();
-    };
-}
-
-// function logBackdropClick() {
-//     if (isOnlyBackdrop) {
-//         console.log("Це клік в бєкдроп");
-//         toggleModal();
-//     };
-//     isOnlyBackdrop = true;
-// }
-
-// console.log("-----------------------------------------------------------------------------------------------------");
-// //! Клавіші-модифікатори
-// console.warn("Клавіші-модифікатори​:");
-// //? ✴️ Для обробки комбінації клавіш,
-// //? наприклад Ctrl + s або будь-якої іншої,
-// //? на об'єкті події є властивості
-// //? ctrlKey, altKey, shiftkey і metaKey,
-// //? що зберігають булеве значення,
-// //? яке сигналізує про те, чи була затиснута клавіша-модифікатор.
-// //? ✴️ Деякі комбінації клавіш 
-// //? можуть конфліктувати з поведінкою браузера за замовчуванням. 
-// //? Наприклад, Ctrl + d або Command + d робить закладку. 
-// //? Потрібно намагатися проектувати систему комбінацій сторінки таким чином,
-// //?  щоб вона не перетиналася із вбудованою у браузері. 
-// //? Але, у крайньому разі, поведінку за замовчуванням можна скасувати, 
-// //? викликавши метод event.preventDefault().
-// document.addEventListener("keydown", event => {
-//     event.preventDefault();
-//     if ((event.ctrlKey || event.metaKey) && event.code === "KeyS") {
-//         console.warn("«Ctrl + s» or «Command + s» combo");
-//     }
-// });
-// Escape
+    function logKeyEscapeClick(event) {
+        if (event.code === "Escape") {
+            console.log("Натиснута клавіша <Escape>");
+            toggleModal();
+        };
+    }
+// })();
