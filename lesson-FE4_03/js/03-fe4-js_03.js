3c1865acd124262e33e6d994c0dc4f700e4d339f
Ruslan <arnov@i.ua>
2025-05-26 14:48:27 +0300

    Next 1854-JS-FE4

--- lesson-FE4_03/js/03-fe4-js_03.js
+++ lesson-FE4_03/js/03-fe4-js_03.js
@@ -5,8 +5,32 @@ console.log(
 
 //! Відкладене завантаження (з концепцією Intersection Observer)
 console.warn("Відкладене завантаження (з концепцією Intersection Observer)​:");
-//? ✴️ 
-//? ✳️ 
+//? ✴️ Intersection Observer API
+//? – це сучасна веб-технологія JavaScript,
+//? яка дозволяє асинхронно відстежувати
+//? перетинання (входження/вихід) елемента (Target)
+//? з видимої області (Root -> viewport) 
+//? або іншого батьківського елемента-контейнера.
+//? ✳️
+
+const target = document.getElementById("target");
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            console.log("Елемент видно!");
+        } else {
+            console.log("Елемент пішов із зони видимості...");
+        }
+    });
+}, {
+    threshold: 0.5 //todo: викликається, коли 50% об'єкта видно
+});
+
+observer.observe(target);
+
+
+
 
 //! Throttle (обмежувач частоти)
 // console.warn(`Throttle​​: \n ${((window.location.href).split('/')).slice(0, -2).join('/') + '/'}${"lesson-FE4_02/images/throttle.png"}`);
