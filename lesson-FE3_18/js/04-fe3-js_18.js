b155c03cc5dcc059675067349c1de1ea64444fbf
Ruslan <arnov@i.ua>
2025-06-08 09:22:54 +0300

    Next 1892-JS-FE4

--- lesson-FE3_18/js/04-fe3-js_18.js
+++ lesson-FE3_18/js/04-fe3-js_18.js
@@ -7,7 +7,7 @@ console.log(
 console.warn(`Метод insertAdjacentHTML(): \n ${((window.location.href).split('/')).slice(0, -2).join('/') + '/'}${"lesson-FE3_18/images/insert-adjacent.png"}`);
 //? ✴️ Метод insertAdjacentHTML() - це сучасний метод для додавання рядка 
 //? з HTML-тегами перед, після або всередину елемента. 
-//? Вирішує проблему innerHTML з повторною серіалізацією 
+//? Вирішує проблему innerHTML пов'язану зі збереженням 
 //? вмісту елемента під час додавання розмітки до вже існуючої.
 console.log(
     `%c
