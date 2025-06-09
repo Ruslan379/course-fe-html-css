32e4e5b25d8406dad441a8fb5b90a38e729fccf0
Ruslan <arnov@i.ua>
2025-06-09 17:54:19 +0300

    Next 1897-JS-FE4

--- lesson-FE4_05/js/03-fe4-js_05.js
+++ lesson-FE4_05/js/03-fe4-js_05.js
@@ -1,29 +1,29 @@
 console.log(
-    '%c 3.Бібліотека PNotify. ',
+    '%c 2.Бібліотека PNotify. ',
     'color: white; background-color: #D33F49',
 );
 
 //! Бібліотека PNotify
-console.warn("Бібліотека PNotify:");
-console.warn(`Зображення​​: \n ${((window.location.href).split('/')).slice(0, -2).join('/') + '/'}${"lesson-FE4_04/images/image-1.png"}`);
-console.warn("Посилання: \n https://caniuse.com/?search=loading%3D%22lazy%22");
-
-//? ✴️
-//? ✳️ 
-
-//?     📌 ---:
-//? 🔸 ООО.
-//? 🔸 ООО.
-//? 🔸 ООО.
+console.warn("Бібліотека PNotify на NPM: \n https://www.npmjs.com/package/pnotify");
+console.warn("Бібліотека PNotify на GitHub: \n https://sciactive.github.io/pnotify/");
 
 
+//! Встановлення бібліотеки PNotify
+console.warn("Встановлення бібліотеки nanoid:")
 console.log(
     `%c
-    <
-        ......
-    />
+    npm i pnotify
     `,
     'color: blue; font-size: 18px',
 );
-
 console.log("-------------------------------------------------------------------");
+
+
+//! Використання бібліотеки PNotify
+console.warn("Використання бібліотеки nanoid:")
+console.log(
+    `%c
+    
+    `,
+    'color: blue; font-size: 18px',
+);
