ddcc1e058c01bb661ca0f3d36d28c2ffa599ae83
Ruslan <arnov@i.ua>
2025-06-09 14:39:08 +0300

    Next 1893-JS-FE4

---
+++ lesson-FE4_05/js/01-fe4-js_05.js
@@ -0,0 +1,111 @@
+console.log(
+    '%c 1.Node.js. Встановлення. ',
+    'color: white; background-color: #D33F49',
+);
+
+//! Встановлення Node.js
+console.warn("Встановлення Node.js: \n https://nodejs.org/en/download/");
+//? ✴️ Node.js — це відкрите середовище виконання JavaScript,
+//? яке дозволяє виконувати JavaScript на стороні сервера.
+//? Це означає, що з Node.js можна створювати серверні додатки,
+//? які можуть обробляти запити від клієнтів, доступ до баз даних,
+//? роботу з файлами, мережеві операції та багато іншого.
+
+//? ✴️ Node.js був створений на основі двигуна V8,
+//? який є основою для браузера Google Chrome.
+//? Це дозволяє виконувати JavaScript з високою швидкістю і ефективністю.
+//? Node.js також має велику кількість розширень (модулів),
+//? які дозволяють легко додавати різноманітні функції до ваших додатків.
+
+
+//! Перевірка версії Node.js
+console.warn("Перевірка версії Node.js:");
+console.log(
+    `%c
+    node -v
+    `,
+    'color: blue; font-size: 18px',
+);
+console.log("--------------------------------------------------------------------------------------");
+
+
+
+//! Створення нового файла test.js
+console.warn("Створення нового файла test.js:");
+console.log(
+    `%c
+    touch test.js
+    `,
+    'color: blue; font-size: 18px',
+);
+console.log("--------------------------------------------------------------------------------------");
+
+
+
+//! Запуск файлу test.js
+console.warn("Запуск файлу test.js:");
+console.log(
+    `%c
+    node test.js
+    `,
+    'color: blue; font-size: 18px',
+);
+console.log("--------------------------------------------------------------------------------------");
+
+
+
+//! Основні команди
+console.warn("Основні команди:");
+console.log(
+    `%c
+    pwd — показує поточну дерикторію, де ви знаходитесь
+    ls — показати які файли і папки
+    clear — очистити термінал або ctrl+l
+    ls -l — показати всі файли
+    ls -la — показати разом з прихованими файлами
+    cd — навгація по папкам
+    cd D: — виходить на диск D
+    touch — створює новий файл
+    mv — переіменовує/переміщує файл або папку
+    rm — видаляє файл або папку
+    mkdir — створює нову папку
+    `,
+    'color: blue; font-size: 18px',
+);
+console.log("--------------------------------------------------------------------------------------");
+
+
+//! JavaScript поза браузером - REPL (read-eval-print loop)
+console.warn("JavaScript поза браузером - REPL (read-eval-print loop):");
+//? ✳️ Node.js дозволяє виконувати JavaScript-код поза браузером.
+//? Відкрийте будь-який термінал і виконайте команду node,
+//? запуститься REPL (read-eval-print loop)
+//? - інтерактивне середовище виконання JS-коду:
+console.log(
+    `%c
+    $ node
+    Welcome to Node.js v20.17.0.
+    Type ".help" for more information.
+    >
+    (To exit, press Ctrl+C again or Ctrl+D or type .exit)
+    >const message = "Hello world!"
+    `,
+    'color: blue; font-size: 18px',
+);
+console.log("--------------------------------------------------------------------------------------");
+
+
+//! Приклад використання REPL (read-eval-print loop)
+console.warn("Приклад використання REPL (read-eval-print loop):");
+console.log(
+    `%c
+    > const message = "Hello world!"
+    undefined
+    > console.log(message)
+    Hello world!
+    undefined
+    >
+    `,
+    'color: blue; font-size: 18px',
+);
+console.log("--------------------------------------------------------------------------------------");
---
+++ lesson-FE4_05/js/03-fe4-js_05.js
@@ -0,0 +1,23 @@
+console.log(
+    '%c 3.Команди NPM. ',
+    'color: white; background-color: #D33F49',
+);
+
+//! Команди NPM
+console.warn("Документація NPM: \n https://docs.npmjs.com/");
+console.warn("Основні команди:");
+console.log(
+    `%c
+    npm init — ініціалізує npm в проєкті і створює файл package.json
+    npm install — встановлює всі залежності, перелічені в package.json
+    npm list --depth=0 — виведе в терміналі список локально встановлених пакетів з номерами їх версій, без залежностей
+    npm install [package-name] — встановить пакет локально у папку node_modules
+    npm uninstall [package-name] — видалить пакет, встановлений локально і оновить package.json
+    npm start і npm test — запустить скрипт start або test, розташований в package.json
+    npm run [custom-script] — запустить кастомний скрипт, розташований в package.json
+    npm outdated — використовується для пошуку оновлень, виявить сумісні версії програмно і виведе список доступних оновлень
+    npm update — оновить всі пакети до максимально дозволеної версії
+    `,
+    'color: blue; font-size: 18px',
+);
+console.log("--------------------------------------------------------------------------------------------------------");
