711487359a60bf6b04a381d82b65f824c9988b44
Ruslan <arnov@i.ua>
2025-06-30 20:29:16 +0300

    Next 1935-JS-FE4

--- lesson-FE4_08/js/04-fe4-js_08.js
+++ lesson-FE4_08/js/04-fe4-js_08.js
@@ -5,8 +5,6 @@ console.log(
 
 //! Обробка помилок
 console.warn("Обробка помилок:");
-console.warn(`Зображення​​: \n ${((window.location.href).split('/')).slice(0, -2).join('/') + '/'}${"lesson-FE4_04/images/image-1.png"}`);
-console.warn("Посилання: \n https://caniuse.com/?search=loading%3D%22lazy%22");
 
 //? ✴️
 //? ✳️ 
