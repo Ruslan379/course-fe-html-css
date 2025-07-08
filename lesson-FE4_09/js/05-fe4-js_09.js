94e466d14d638bf6022167520da15e020f081cb2
Ruslan <arnov@i.ua>
2025-07-08 16:51:01 +0300

    Next 1949-JS-FE4

--- lesson-FE4_09/js/05-fe4-js_09.js
+++ lesson-FE4_09/js/05-fe4-js_09.js
@@ -14,27 +14,33 @@ console.log(
     localStorage.setItem("ui-theme", "light");
     localStorage.setItem("sidebar", "expanded");
     localStorage.setItem("notification-level", "mute");
+
     console.log(localStorage.getItem("ui-theme")); //! "light"
     console.log(localStorage.getItem("sidebar")); //! "expanded"
     console.log(localStorage.getItem("notification-level")); //! "mute"
     
     localStorage.clear();
+
     console.log(localStorage.getItem("ui-theme")); //! null
     console.log(localStorage.getItem("sidebar")); //! null
     console.log(localStorage.getItem("notification-level")); //! null
     `,
     'color: blue; font-size: 18px',
 );
+console.log(". . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .");
 
 localStorage.setItem("ui-theme", "light");
 localStorage.setItem("sidebar", "expanded");
 localStorage.setItem("notification-level", "mute");
-console.log(localStorage.getItem("ui-theme")); //! "light"
-console.log(localStorage.getItem("sidebar")); //! "expanded"
-console.log(localStorage.getItem("notification-level")); //! "mute"
+
+console.log('localStorage.getItem("ui-theme"):', localStorage.getItem("ui-theme")); //! "light"
+console.log('localStorage.getItem("sidebar"):', localStorage.getItem("sidebar")); //! "expanded"
+console.log('localStorage.getItem("notification-level"):', localStorage.getItem("notification-level")); //! "mute"
+console.log(". . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .");
 
 localStorage.clear();
-console.log(localStorage.getItem("ui-theme")); //! null
-console.log(localStorage.getItem("sidebar")); //! null
-console.log(localStorage.getItem("notification-level")); //! null
-console.log("-------------------------------------------------------------------");
+
+console.log('localStorage.getItem("ui-theme"):', localStorage.getItem("ui-theme")); //! null
+console.log('localStorage.getItem("sidebar"):', localStorage.getItem("sidebar")); //! null
+console.log('localStorage.getItem("notification-level"):', localStorage.getItem("notification-level")); //! null
+console.log("-------------------------------------------------------------------------------------------------------------");
