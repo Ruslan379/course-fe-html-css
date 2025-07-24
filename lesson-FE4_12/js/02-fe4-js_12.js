57a93b790148f8b337c41904884ec0174be5d8b8
Ruslan <arnov@i.ua>
2025-07-24 19:28:15 +0300

    Next 1973-JS-FE4

--- lesson-FE4_12/js/02-fe4-js_12.js
+++ lesson-FE4_12/js/02-fe4-js_12.js
@@ -5,8 +5,6 @@ console.log(
 
 //! Створення дати. 7 аргументів.
 console.warn("Створення дати. 7 аргументів.:");
-console.warn(`Зображення​​: \n ${((window.location.href).split('/')).slice(0, -2).join('/') + '/'}${"lesson-FE4_04/images/image-1.png"}`);
-console.warn("Посилання: \n https://caniuse.com/?search=loading%3D%22lazy%22");
 
 //? ✴️
 //? ✳️ 
--- lesson-FE4_12/js/04-fe4-js_12.js
+++ lesson-FE4_12/js/04-fe4-js_12.js
@@ -1,12 +1,10 @@
 console.log(
-    '%c 1.Module-1. ',
+    '%c 4.Методи Date. Геттери. ',
     'color: white; background-color: #D33F49',
 );
 
-//! Module-1
-console.warn("Module-1:");
-console.warn(`Зображення​​: \n ${((window.location.href).split('/')).slice(0, -2).join('/') + '/'}${"lesson-FE4_04/images/image-1.png"}`);
-console.warn("Посилання: \n https://caniuse.com/?search=loading%3D%22lazy%22");
+//! Методи Date. Геттери
+console.warn("Методи Date. Геттери:");
 
 //? ✴️
 //? ✳️ 
