0e48c34acd2f07caa419226e2f0305decb1e2e76
Ruslan <arnov@i.ua>
2025-07-24 18:25:24 +0300

    Next 1972-JS-FE4

--- lesson-FE4_12/js/01-fe4-js_12.js
+++ lesson-FE4_12/js/01-fe4-js_12.js
@@ -1,12 +1,10 @@
 console.log(
-    '%c 1.Module-1. ',
+    '%c 1.Створення дати. Клас Date. ',
     'color: white; background-color: #D33F49',
 );
 
-//! Module-1
-console.warn("Module-1:");
-console.warn(`Зображення​​: \n ${((window.location.href).split('/')).slice(0, -2).join('/') + '/'}${"lesson-FE4_04/images/image-1.png"}`);
-console.warn("Посилання: \n https://caniuse.com/?search=loading%3D%22lazy%22");
+//! Створення дати. Клас Date
+console.warn("Створення дати. Клас Date:");
 
 //? ✴️
 //? ✳️ 
