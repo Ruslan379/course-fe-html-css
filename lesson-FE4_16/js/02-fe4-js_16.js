cf0532bc394945bcb460648722d05a04c905ddcd
Ruslan <arnov@i.ua>
2025-08-31 19:16:19 +0300

    Next 2053-JS-FE4

---
+++ lesson-FE4_16/js/02-fe4-js_16.js
@@ -0,0 +1,29 @@
+console.log(
+    '%c 2.Fetch API​ ',
+    'color: white; background-color: #D33F49',
+);
+
+//! Fetch API​ 
+console.warn("Fetch API​ :");
+console.warn(`Зображення​​: \n ${((window.location.href).split('/')).slice(0, -2).join('/') + '/'}${"lesson-FE4_04/images/image-1.png"}`);
+console.warn("Посилання: \n https://caniuse.com/?search=loading%3D%22lazy%22");
+
+//? ✴️
+//? ✳️ 
+
+//?     📌 ---:
+//? 🔸 ООО.
+//? 🔸 ООО.
+//? 🔸 ООО.
+
+
+console.log(
+    `%c
+    <
+        ......
+    />
+    `,
+    'color: blue; font-size: 18px',
+);
+
+console.log("-------------------------------------------------------------------");
---
+++ lesson-FE4_16/js/04-fe4-js_16.js
@@ -0,0 +1,37 @@
+console.log(
+    '%c 4.Вкладка Network.             \n   Робота з публічним REST API. ',
+    'color: white; background-color: #D33F49',
+);
+
+//! Вкладка Network
+console.warn("Вкладка Network:");
+console.warn(`Зображення​​: \n ${((window.location.href).split('/')).slice(0, -2).join('/') + '/'}${"lesson-FE4_04/images/image-1.png"}`);
+console.warn("Посилання: \n https://caniuse.com/?search=loading%3D%22lazy%22");
+
+//? ✴️
+//? ✳️ 
+
+//?     📌 ---:
+//? 🔸 ООО.
+//? 🔸 ООО.
+//? 🔸 ООО.
+
+
+console.log(
+    `%c
+    <
+        ......
+    />
+    `,
+    'color: blue; font-size: 18px',
+);
+
+console.log("-------------------------------------------------------------------");
+
+
+//! Робота з публічним REST API.
+console.warn("Робота з публічним REST API:");
+
+
+
+console.log("-------------------------------------------------------------------");
