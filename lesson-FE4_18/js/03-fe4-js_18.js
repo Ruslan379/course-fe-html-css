2b982cdcb550ac30b2ee50a61579607119f474c9
Ruslan <103874267+Ruslan379@users.noreply.github.com>
2025-09-20 09:20:01 +0300

    Next 2082-JS-FE4

--- lesson-FE4_18/js/03-fe4-js_18.js
+++ lesson-FE4_18/js/03-fe4-js_18.js
@@ -41,7 +41,7 @@ const requestBody = {
 };
 
 const options = {
-    method: "POST",
+    method: "POST", //! операція CREATE, створення
     body: JSON.stringify(requestBody), //! конвертація даних у JSON-формат
     headers: {
         "Content-Type": "application/json; charset=UTF-8",
@@ -50,9 +50,9 @@ const options = {
 
 fetch("https://jsonplaceholder.typicode.com/posts", options)
     .then(response => response.json())
-    .then(res => console.log("Відповідь сервера:", res))
+    .then(res => console.log("Відповідь сервера на POST:", res))
     .catch(error => console.log(error))
-    .finally(() => console.log("----------------------------------------------------------------------------------"));
+    .finally(() => console.log("-------------------------------------------------------------------------------------------"));
 
 //? ✳️ Робимо запит на створення поста,
 //? звертаючись до ресурсу /posts,
@@ -70,4 +70,15 @@ fetch("https://jsonplaceholder.typicode.com/posts", options)
 //? або задати інший тип (дивись вище).
 //? Не забуваємо вказати заголовок Content-Type,
 //? який уточнює для бекенду тип переданих даних.
-// console.log("----------------------------------------------------------------------------------");
+
+
+//! Meтод GET. Операція READ. Читання
+//? ☹️ Але, JSONPlaceholder API, на жаль, фактично не змінює дані після запиту:
+// setTimeout(() => {
+//     fetch(`https://jsonplaceholder.typicode.com/posts/101`)
+//         .then(response => response.json())
+//         .then(post => console.log(`post №101 після POST:`, post))
+//         .catch(error => console.log(error))
+//         .finally(() => console.log("-------------------------------------------------------------------------------------------"));
+// }, 500);
+// console.log("-------------------------------------------------------------------------------------------"));
