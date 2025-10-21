397c7ffb9e4781d51057d44039bc0ed4430035e5
Ruslan <103874267+Ruslan379@users.noreply.github.com>
2025-10-21 10:25:16 +0300

    Next 2115-JS-FE4

---
+++ lesson-FE4_20/js/01-fe4-js_20.js
@@ -0,0 +1,8 @@
+console.log(
+    '%c 1.Загальний вигляд проєкту. ',
+    'color: white; background-color: #D33F49',
+);
+
+//! Загальний вигляд проєкту
+console.warn(`Загальний вигляд проєкту​: \n ${((window.location.href).split('/')).slice(0, -2).join('/') + '/'}${"lesson-FE4_20/images/hw-20.jpg"}`);
+console.log("-------------------------------------------------------------------");
---
+++ lesson-FE4_20/js/05-fe4-js_20.js
@@ -0,0 +1,29 @@
+console.log(
+    '%c 1.Module-1. ',
+    'color: white; background-color: #D33F49',
+);
+
+//! Module-1
+console.warn("Module-1:");
+console.warn(`Зображення​​: \n ${((window.location.href).split('/')).slice(0, -2).join('/') + '/'}${"lesson-FE4_04/images/image-1.png"}`);
+console.warn("Посилання: \n https://caniuse.com/?search=loading%3D%22lazy%22");
+
+//? ✴️
+//? ✳️ 
+
+//?     📌 ---:
+//? 🔸 ООО.
+//? 🔸 ООО.
+//? 🔸 ООО.
+
+
+console.log(
+    `%c
+    <
+        ......
+    />
+    `,
+    'color: blue; font-size: 18px',
+);
+
+console.log("-------------------------------------------------------------------");
