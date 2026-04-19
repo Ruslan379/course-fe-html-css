399d93d7cc8b122eddd014417cfc4444f3eac6cc
Ruslan <arnov@i.ua>
2026-04-19 13:52:06 +0300

    Next 2145-JS-FE4

--- lesson-FE3_08/js/10-practice.js
+++ lesson-FE3_08/js/10-practice.js
@@ -71,3 +71,43 @@ console.log("finalArr:", finalArr);
 // i = 2, j = 6 --- 10
 // i = 3, j = 11 --- 13
 console.log(". . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .");
+
+
+//! var-1. Павло
+console.log(
+  "%c [2] var-1. Павло ",
+  "color: yellow; background-color: #2274A5",
+);
+const numArray2 = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120, 130, 140, 150, 160, 170, 180, 190, 200, 22, 34, 44]
+let resultArray = [];
+
+for (let i = 0; i < numArray2.length; i += 5) {
+  let pieceArray = numArray2.slice(i, i + 5);
+  resultArray.push(pieceArray);
+};
+
+console.log("resultArray:", resultArray);
+console.log(". . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .");
+
+
+//! var-2. Павло
+console.log(
+  "%c [2] var-2. Павло ",
+  "color: yellow; background-color: #2274A5",
+);
+const numArray3 = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120, 130, 140, 150, 160, 170, 180, 190, 200, 22, 34, 44]
+
+let resultArray3 = [];
+let arraySize = 5;
+let numOfElements3 = 0;
+
+for (let i = 0; numOfElements3 < numArray3.length; i += 1) {
+  let temp = [];
+  for (let value of numArray3.slice(numOfElements3, numOfElements3 + arraySize)) {
+    temp.push(value);
+  }
+  resultArray3.push(temp)
+  numOfElements3 += arraySize
+};
+console.log("resultArray3:", resultArray3)
+console.log(". . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .");
